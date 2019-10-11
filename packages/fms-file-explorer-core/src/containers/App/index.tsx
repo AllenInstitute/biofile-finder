a4bed40f9c92dcf42ff99ea6b6f035645e16029e
Gabe Medrash <gabeme@alleninstitute.org>
2019-10-11 09:49:41 -0700

    [FMS-766] Compose placeholder containers and layout them out

--- packages/fms-file-explorer-core/src/containers/App/index.tsx
+++ packages/fms-file-explorer-core/src/containers/App/index.tsx
@@ -1,9 +1,27 @@
 import * as React from "react";
 
+import AnnotationHierarchy from "../AnnotationHierarchy";
+import Breadcrumbs from "../Breadcrumbs";
+import FileDetails from "../FileDetails";
+import FileList from "../FileList";
+import HeaderRibbon from "../HeaderRibbon";
+
 const styles = require("./style.module.css");
 
-export default class App extends React.Component<{}, {}> {
-    public render(): JSX.Element {
-        return <div className={styles.container}>Hello world</div>;
-    }
+export default function App() {
+    return (
+        <div className={styles.root}>
+            <HeaderRibbon className={styles.headerRibbon} />
+            <div className={styles.everythingExceptHeaderRibbon}>
+                <div className={styles.core}>
+                    <Breadcrumbs className={styles.breadcrumbs} />
+                    <div className={styles.annotationHierarchyAndFileList}>
+                        <AnnotationHierarchy className={styles.annotationHierarchy} />
+                        <FileList className={styles.fileList} />
+                    </div>
+                </div>
+                <FileDetails className={styles.fileDetails} />
+            </div>
+        </div>
+    );
 }
