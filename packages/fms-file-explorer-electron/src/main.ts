aededaa67b7599f78ad617ff5270484b4891a22e
Gabe Medrash <gabeme@alleninstitute.org>
2019-10-11 09:44:25 -0700

    Add electron-devtools-installer and pull in React devtools on dev build

--- packages/fms-file-explorer-electron/src/main.ts
+++ packages/fms-file-explorer-electron/src/main.ts
@@ -1,5 +1,7 @@
-import { app, BrowserWindow } from "electron";
 import * as path from "path";
+
+import { app, BrowserWindow } from "electron";
+import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 
@@ -17,18 +19,30 @@ const createMainWindow = () => {
     });
 
     if (isDevelopment) {
-        mainWindow
-            .loadURL(`http://localhost:${process.env.WEBPACK_DEV_SERVER_PORT}`)
-            .then(() => {
-                console.log("Loaded from webpack-dev-server");
+        installExtension(REACT_DEVELOPER_TOOLS)
+            .then((name: string) => {
+                console.log(`Added extension: ${name}`);
 
-                if (mainWindow) {
-                    mainWindow.webContents.openDevTools();
+                if (!mainWindow) {
+                    throw new Error("mainWindow not defined");
                 }
+
+                mainWindow
+                    .loadURL(`http://localhost:${process.env.WEBPACK_DEV_SERVER_PORT}`)
+                    .then(() => {
+                        console.log("Loaded from webpack-dev-server");
+
+                        if (mainWindow) {
+                            mainWindow.webContents.openDevTools();
+                        }
+                    })
+                    .catch((error: Error) => {
+                        console.error("Failed to load from webpack-dev-server", error);
+                    });
             })
-            .catch((error: Error) => {
-                console.error("Failed to load from webpack-dev-server", error);
-            });
+            .catch((err: Error) =>
+                console.error("An error occurred loading React Dev Tools: ", err)
+            );
     } else {
         mainWindow
             .loadURL(`file://${path.join(__dirname, "renderer", "index.html")}`)
@@ -39,24 +53,23 @@ const createMainWindow = () => {
                 console.error("Failed to load from file", error);
             });
     }
-}
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createMainWindow);
-
+app.on("ready", createMainWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
 
-app.on('activate', function () {
+app.on("activate", function() {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
