8522d0f331775f17bf87277fb51eedb5cc3a9fca
Gabe Medrash <gabeme@alleninstitute.org>
2019-10-15 12:04:38 -0700

    [FMS-768] Add entity to work with responses that match the interface returned from api-response-java

---
+++ packages/fms-file-explorer-core/src/entity/RestServiceResponse/index.ts
@@ -0,0 +1,39 @@
+interface Response<T> {
+    data: T[];
+    hasMore: boolean;
+    offset: number;
+    responseType: string;
+    totalCount: number;
+}
+
+/**
+ * A response from an AICS backend service. Intended to match the interface provided by the `api-response-java` library.
+ * See https://aicsbitbucket.corp.alleninstitute.org/projects/SW/repos/api-response-java/browse.
+ */
+export default class RestServiceResponse<T = any> {
+    private readonly response: Response<T>;
+
+    constructor(response: Response<T>) {
+        this.response = response;
+    }
+
+    get data(): T[] {
+        return this.response.data;
+    }
+
+    get hasMore(): boolean {
+        return this.response.hasMore;
+    }
+
+    get offset(): number {
+        return this.response.offset;
+    }
+
+    get responseType(): string {
+        return this.response.responseType;
+    }
+
+    get totalCount(): number {
+        return this.response.totalCount;
+    }
+}
