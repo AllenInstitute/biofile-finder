62d9c5d77e04c30c623eb6990efef1bcb0bbdcb3
Gabe Medrash <gabeme@alleninstitute.org>
2019-10-18 15:51:26 -0700

    [setup] Add regenerator-runtime to support async functions

--- packages/fms-file-explorer-electron/src/renderer.tsx
+++ packages/fms-file-explorer-electron/src/renderer.tsx
@@ -1,3 +1,5 @@
+import "regenerator-runtime/runtime";
+
 import FmsFileExplorer, { createReduxStore } from "fms-file-explorer-core";
 import * as React from "react";
 import { render } from "react-dom";
