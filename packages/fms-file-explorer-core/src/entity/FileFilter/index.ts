930f28cb059acc02817b83e722f613efabfd72b0
Gabe Medrash <gabeme@alleninstitute.org>
2019-11-06 10:11:11 -0800

    [FMS-807] FileFilter entity stub

---
+++ packages/fms-file-explorer-core/src/entity/FileFilter/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Stub for a filter used to constrain a listing of files to those that match a particular condition. Should be
+ * serializable to a URL query string-friendly format.
+ */
+export default class FileFilter {}
