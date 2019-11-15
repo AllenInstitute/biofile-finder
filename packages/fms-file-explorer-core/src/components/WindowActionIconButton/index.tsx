90cf225551cd875d6524254ce0761c4f9ea4b0ec
Gabe Medrash <gabeme@alleninstitute.org>
2019-11-15 15:16:48 -0800

    [FMS-769] Put path data into a mapping and simplify WindowAction enum

--- packages/fms-file-explorer-core/src/components/WindowActionIconButton/index.tsx
+++ packages/fms-file-explorer-core/src/components/WindowActionIconButton/index.tsx
@@ -2,14 +2,10 @@ import * as React from "react";
 
 const styles = require("./WindowActionIconButton.module.css");
 
-/**
- * Path data for icons taken from Material Design
- * Apache License 2.0 (https://github.com/google/material-design-icons/blob/master/LICENSE)
- */
 export enum WindowAction {
-    MINIMIZE = "M13 3.25C13 3.25 0 3.25 0 3.25 0 3.25 0 0 0 0 0 0 13 0 13 0",
-    RESTORE = "M0 3.25C0 3.25 3.25 3.25 3.25 3.25 3.25 3.25 3.25 0 3.25 0 3.25 0 13 0 13 0 13 0 13 9.75 13 9.75 13 9.75 9.75 9.75 9.75 9.75 9.75 9.75 9.75 13 9.75 13 9.75 13 0 13 0 13 0 13 0 3.25 0 3.25M9.75 3.25C9.75 3.25 9.75 8.125 9.75 8.125 9.75 8.125 11.375 8.125 11.375 8.125 11.375 8.125 11.375 1.625 11.375 1.625 11.375 1.625 4.875 1.625 4.875 1.625 4.875 1.625 4.875 3.25 4.875 3.25 4.875 3.25 9.75 3.25 9.75 3.25M1.625 6.5C1.625 6.5 1.625 11.375 1.625 11.375 1.625 11.375 8.125 11.375 8.125 11.375 8.125 11.375 8.125 6.5 8.125 6.5 8.125 6.5 1.625 6.5 1.625 6.5 1.625 6.5 1.625 6.5 1.625 6.5",
-    MAXIMIZE = "M0 0C0 0 13 0 13 0 13 0 13 13 13 13 13 13 0 13 0 13 0 13 0 0 0 0M1.625 3.25C1.625 3.25 1.625 11.375 1.625 11.375 1.625 11.375 11.375 11.375 11.375 11.375 11.375 11.375 11.375 3.25 11.375 3.25 11.375 3.25 1.625 3.25 1.625 3.25 1.625 3.25 1.625 3.25 1.625 3.25",
+    MINIMIZE = "MINIMIZE",
+    RESTORE = "RESTORE",
+    MAXIMIZE = "MAXIMIZE",
 }
 
 interface WindowActionIconButtonProps {
@@ -19,6 +15,18 @@ interface WindowActionIconButtonProps {
     onClick?: () => void;
     width?: number; // px
 }
+
+/**
+ * Path data for icons taken from Material Design
+ * Apache License 2.0 (https://github.com/google/material-design-icons/blob/master/LICENSE)
+ */
+const actionToPathDataMap: { [index: string]: string } = {
+    [WindowAction.MINIMIZE]: "M13 3.25C13 3.25 0 3.25 0 3.25 0 3.25 0 0 0 0 0 0 13 0 13 0",
+    [WindowAction.RESTORE]:
+        "M0 3.25C0 3.25 3.25 3.25 3.25 3.25 3.25 3.25 3.25 0 3.25 0 3.25 0 13 0 13 0 13 0 13 9.75 13 9.75 13 9.75 9.75 9.75 9.75 9.75 9.75 9.75 9.75 13 9.75 13 9.75 13 0 13 0 13 0 13 0 3.25 0 3.25M9.75 3.25C9.75 3.25 9.75 8.125 9.75 8.125 9.75 8.125 11.375 8.125 11.375 8.125 11.375 8.125 11.375 1.625 11.375 1.625 11.375 1.625 4.875 1.625 4.875 1.625 4.875 1.625 4.875 3.25 4.875 3.25 4.875 3.25 9.75 3.25 9.75 3.25M1.625 6.5C1.625 6.5 1.625 11.375 1.625 11.375 1.625 11.375 8.125 11.375 8.125 11.375 8.125 11.375 8.125 6.5 8.125 6.5 8.125 6.5 1.625 6.5 1.625 6.5 1.625 6.5 1.625 6.5 1.625 6.5",
+    [WindowAction.MAXIMIZE]:
+        "M0 0C0 0 13 0 13 0 13 0 13 13 13 13 13 13 0 13 0 13 0 13 0 0 0 0M1.625 3.25C1.625 3.25 1.625 11.375 1.625 11.375 1.625 11.375 11.375 11.375 11.375 11.375 11.375 11.375 11.375 3.25 11.375 3.25 11.375 3.25 1.625 3.25 1.625 3.25 1.625 3.25 1.625 3.25 1.625 3.25",
+};
 
 // Necessary to account (in the viewBox) for the width/height of the SVGs as they were designed so that the icons scale
 // properly
@@ -40,7 +48,7 @@ export default function WindowActionIconButton(props: WindowActionIconButtonProp
                 width={width}
             ></rect>
             <path
-                d={action}
+                d={actionToPathDataMap[action]}
                 strokeWidth="0"
                 stroke="none"
                 strokeDasharray="none"
