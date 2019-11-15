9d4b7b74c1dc8c449d1dd9e09f05950fd09a13c3
Gabe Medrash <gabeme@alleninstitute.org>
2019-11-15 15:17:15 -0800

    [FMS-769] windowStateReducer

---
+++ packages/fms-file-explorer-core/src/containers/FileDetails/windowStateReducer.ts
@@ -0,0 +1,46 @@
+import { WindowAction } from "../../components/WindowActionIconButton";
+
+export enum WindowState {
+    DEFAULT,
+    MINIMIZED,
+    MAXIMIZED,
+}
+
+export const INITIAL_STATE = {
+    state: WindowState.DEFAULT,
+    possibleActions: [WindowAction.MINIMIZE, WindowAction.MAXIMIZE],
+};
+
+type State = { state: WindowState; possibleActions: WindowAction[] };
+type Action = { type: WindowAction };
+
+/**
+ * Small state machine to manage the "window state" of the details pane. Window state can be:
+ * - MINIMIZED, in which case we need to be able to:
+ *      - restore it to its default size
+ *      - maximize it
+ * - MAXIMIZED, in which case we need to be able to:
+ *      - restore it to its default size
+ *      - minimize it
+ * - DEFAULT, in which case we need to be able to:
+ *      - minimize it
+ *      - maximize it
+ */
+export default function windowStateReducer(state: State, action: Action): State {
+    switch (action.type) {
+        case WindowAction.MINIMIZE:
+            return {
+                state: WindowState.MINIMIZED,
+                possibleActions: [WindowAction.RESTORE, WindowAction.MAXIMIZE],
+            };
+        case WindowAction.MAXIMIZE:
+            return {
+                state: WindowState.MAXIMIZED,
+                possibleActions: [WindowAction.MINIMIZE, WindowAction.RESTORE],
+            };
+        case WindowAction.RESTORE:
+        // prettier-ignore
+        default: // FALL THROUGH
+            return INITIAL_STATE;
+    }
+}
