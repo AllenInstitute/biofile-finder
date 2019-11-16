7515d663adbcaf7f38c545ce5fa766ada531a54a
Gabe Medrash <gabeme@alleninstitute.org>
2019-11-15 17:03:16 -0800

    [FMS-769] Render WindowActionIconButtons into FileDetails

--- packages/fms-file-explorer-core/src/containers/FileDetails/index.tsx
+++ packages/fms-file-explorer-core/src/containers/FileDetails/index.tsx
@@ -2,8 +2,10 @@ import * as classNames from "classnames";
 import * as React from "react";
 import { useSelector } from "react-redux";
 
+import WindowActionIconButton from "../../components/WindowActionIconButton";
 import { selection } from "../../state";
 import useFileDetails from "./useFileDetails";
+import windowStateReducer, { INITIAL_STATE, WindowState } from "./windowStateReducer";
 
 const styles = require("./style.module.css");
 
@@ -11,15 +13,32 @@ interface FileDetails {
     className?: string;
 }
 
+const windowStateToWidthMap: { [index: string]: string } = {
+    [WindowState.MINIMIZED]: "30px",
+    [WindowState.MAXIMIZED]: "100vw",
+};
+
 /**
  * Right-hand sidebar of application. Displays details of selected file(s).
  */
 export default function FileDetails(props: FileDetails) {
+    const [windowState, dispatch] = React.useReducer(windowStateReducer, INITIAL_STATE);
+
     const fileToDetail = useSelector(selection.selectors.getFirstSelectedFile);
     const [fileDetails, isLoading] = useFileDetails(fileToDetail);
 
     return (
-        <div className={classNames(styles.root, props.className)}>
+        <div
+            className={classNames(styles.root, props.className)}
+            style={{ width: windowStateToWidthMap[windowState.state] }}
+        >
+            {windowState.possibleActions.map((action) => (
+                <WindowActionIconButton
+                    key={action}
+                    action={action}
+                    onClick={() => dispatch({ type: action })}
+                />
+            ))}
             {isLoading ? "Loading..." : JSON.stringify(fileDetails, undefined, 4)}
         </div>
     );
