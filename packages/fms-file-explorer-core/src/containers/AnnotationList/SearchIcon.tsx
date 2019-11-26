9c0efa034e661c6d931ea5b73c905d9a7f5ae779
Gabe Medrash <gabeme@alleninstitute.org>
2019-11-25 17:05:36 -0800

    [FMS-860] Enable the annotation search box

---
+++ packages/fms-file-explorer-core/src/containers/AnnotationList/SearchIcon.tsx
@@ -0,0 +1,25 @@
+import * as classNames from "classnames";
+import * as React from "react";
+
+interface SearchIconProps {
+    className?: string;
+    height: number;
+    width: number;
+}
+
+/**
+ * Path data for icons taken from Material Design
+ * Apache License 2.0 (https://github.com/google/material-design-icons/blob/master/LICENSE)
+ */
+export default function SearchIcon({ className, height, width }: SearchIconProps) {
+    return (
+        <svg className={classNames(className)} height={height} width={width} viewBox="0 0 24 24">
+            <path d="M9.516 14.016q1.875 0 3.188-1.313t1.313-3.188-1.313-3.188-3.188-1.313-3.188 1.313-1.313 3.188 1.313 3.188 3.188 1.313zM15.516 14.016l4.969 4.969-1.5 1.5-4.969-4.969v-0.797l-0.281-0.281q-1.781 1.547-4.219 1.547-2.719 0-4.617-1.875t-1.898-4.594 1.898-4.617 4.617-1.898 4.594 1.898 1.875 4.617q0 0.984-0.469 2.227t-1.078 1.992l0.281 0.281h0.797z"></path>
+        </svg>
+    );
+}
+
+SearchIcon.defaultProps = {
+    height: 17,
+    width: 17,
+};
--- packages/fms-file-explorer-core/src/containers/AnnotationList/index.tsx
+++ packages/fms-file-explorer-core/src/containers/AnnotationList/index.tsx
@@ -1,15 +1,28 @@
 import * as classNames from "classnames";
+import * as Fuse from "fuse.js";
 import * as React from "react";
 import { useSelector } from "react-redux";
 
 import List from "./List";
 import * as annotationListSelectors from "./selectors";
+import SearchIcon from "./SearchIcon";
 
 const styles = require("./AnnotationList.module.css");
 
 interface AnnotationListProps {
     className?: string;
 }
+
+const FUZZY_SEARCH_OPTIONS = {
+    // which keys on ListItemData to search
+    keys: [{ name: "title", weight: 0.7 }, { name: "description", weight: 0.3 }],
+
+    // return resulting matches sorted
+    shouldSort: true,
+
+    // arbitrarily tuned; 0.0 requires a perfect match, 1.0 would match anything
+    threshold: 0.2,
+};
 
 /**
  * Listing of all metadata annotations (a.k.a., "keys", "attributes", etc). Users can filter the list using the
@@ -18,14 +31,37 @@ interface AnnotationListProps {
  */
 export default function AnnotationList(props: AnnotationListProps) {
     const annotationListItems = useSelector(annotationListSelectors.getAnnotationListItems);
+    const [searchValue, setSearchValue] = React.useState("");
+
+    // Perform fuzzy search using searchValue within annotation list items, considering the items
+    // title and description. If no searchValue has been entered, return full list of items.
+    const filteredListItems = React.useMemo(() => {
+        if (!searchValue) {
+            return annotationListItems;
+        }
+
+        const fuse = new Fuse(annotationListItems, FUZZY_SEARCH_OPTIONS);
+        return fuse.search(searchValue);
+    }, [annotationListItems, searchValue]);
 
     return (
         <div className={classNames(styles.root, props.className)}>
             <h3 className={styles.title}>Available annotations</h3>
             <h6 className={styles.description}>Drag any annotation to the box above</h6>
             <div className={styles.listContainer}>
-                <input className={styles.filterInput} spellCheck={false} type="text" />
-                <List className={styles.list} items={annotationListItems} />
+                <div className={styles.searchBox}>
+                    <SearchIcon className={styles.searchIcon} />
+                    <input
+                        className={styles.filterInput}
+                        spellCheck={false}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                            setSearchValue(event.target.value);
+                        }}
+                        placeholder="Search..."
+                        type="search"
+                    />
+                </div>
+                <List className={styles.list} items={filteredListItems} />
             </div>
         </div>
     );
