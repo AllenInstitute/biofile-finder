9ce4a68c69a369f1006f9553146d35226eb135dc
Gabe Medrash <gabeme@alleninstitute.org>
2019-11-26 16:38:33 -0800

    [FMS-859] Render the annotation description as a tooltip on top of an info icon

--- packages/fms-file-explorer-core/src/containers/AnnotationList/ListItem.tsx
+++ packages/fms-file-explorer-core/src/containers/AnnotationList/ListItem.tsx
@@ -1,8 +1,14 @@
+import Tippy from "@tippy.js/react";
+import "tippy.js/dist/tippy.css"; // side-effect
 import * as React from "react";
+
+import SvgIcon from "../../components/SvgIcon";
+
+const styles = require("./ListItem.module.css");
 
 export interface ListItemData {
     id: string | number;
-    description?: string;
+    description: string;
     title: any;
 }
 
@@ -10,13 +16,30 @@ interface ListItemProps {
     data: ListItemData;
 }
 
+// Designed Daniel Bruce (www.entypo.com)
+// License: https://creativecommons.org/licenses/by-sa/4.0/
+const INFO_ICON_PATH_DATA =
+    "M12.432 0c1.34 0 2.010 0.912 2.010 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-0.75-1.974-1.99 0-1.043 0.881-2.479 2.643-2.479zM8.309 20c-1.058 0-1.833-0.652-1.093-3.524l1.214-5.092c0.211-0.814 0.246-1.141 0-1.141-0.317 0-1.689 0.562-2.502 1.117l-0.528-0.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273 0.705 3.23l-1.391 5.352c-0.246 0.945-0.141 1.271 0.106 1.271 0.317 0 1.357-0.392 2.379-1.207l0.6 0.814c-2.502 2.547-5.235 3.527-6.291 3.527z";
+
 /**
  * Fundamental list item component rendered by List. Separated from List simply to keep files small and components as
  * single-purpose as possible.
  */
 export default function ListItem(props: ListItemProps) {
-    const {
-        data: { title },
-    } = props;
-    return <li>{title}</li>;
+    const { data } = props;
+
+    return (
+        <li>
+            <Tippy content={data.description}>
+                <SvgIcon
+                    className={styles.info}
+                    height={10}
+                    pathData={INFO_ICON_PATH_DATA}
+                    viewBox="0 0 20 20"
+                    width={10}
+                />
+            </Tippy>
+            {data.title}
+        </li>
+    );
 }
