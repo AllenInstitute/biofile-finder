3de621048ead41d74268cd1725a9c9639dbdac5c
Gabe Medrash <gabeme@alleninstitute.org>
2019-11-26 16:35:14 -0800

    [FMS-859] Forward ref from SvgIcon

--- packages/fms-file-explorer-core/src/components/SvgIcon/index.tsx
+++ packages/fms-file-explorer-core/src/components/SvgIcon/index.tsx
@@ -12,23 +12,26 @@ interface SvgIcon {
 /**
  * A generalized SVG icon.
  */
-export default function SvgIcon(props: SvgIcon) {
+const SvgIcon = React.forwardRef<SVGSVGElement, SvgIcon>((props, ref) => {
     const { className, height, pathAttrs, pathData, viewBox, width } = props;
     return (
         <svg
             className={className}
             height={height}
             preserveAspectRatio="xMidYMid"
+            ref={ref}
             viewBox={viewBox}
             width={width}
         >
             <path {...pathAttrs} d={pathData}></path>
         </svg>
     );
-}
+});
 
 SvgIcon.defaultProps = {
     height: 15,
     pathAttrs: {},
     width: 15,
 };
+
+export default SvgIcon;
