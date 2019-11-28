6e2e2ace5ecd369ecb25e780e345376dda4fb239
Gabe Medrash <gabeme@alleninstitute.org>
2019-11-27 17:04:03 -0800

    [FMS-770] Make SvgIcon optionally interactive looking and accept an onClick handler

--- packages/fms-file-explorer-core/src/components/SvgIcon/index.tsx
+++ packages/fms-file-explorer-core/src/components/SvgIcon/index.tsx
@@ -1,9 +1,13 @@
+import * as classNames from "classnames";
 import { defaults } from "lodash";
 import * as React from "react";
+
+const styles = require("./SvgIcon.module.css");
 
 interface SvgIconProps {
     className?: string;
     height?: number | string;
+    onClick?: () => void;
     pathAttrs?: React.SVGProps<SVGPathElement>;
     pathData: string;
     width?: number | string;
@@ -21,14 +25,15 @@ const DEFAULT_PROPS = {
  * A generalized SVG icon.
  */
 function SvgIcon(props: SvgIconProps, ref?: React.Ref<SVGSVGElement>) {
-    const { className, height, pathAttrs, pathData, viewBox, width } = defaults(
+    const { className, height, onClick, pathAttrs, pathData, viewBox, width } = defaults(
         props,
         DEFAULT_PROPS
     );
     return (
         <svg
-            className={className}
+            className={classNames({ [styles.interactive]: onClick !== undefined }, className)}
             height={height}
+            onClick={onClick}
             preserveAspectRatio="xMidYMid"
             ref={ref}
             viewBox={viewBox}
