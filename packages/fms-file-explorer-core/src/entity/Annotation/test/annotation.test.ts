dc80af99cf97282fe6018862353ee0955b98351b
Gabe Medrash <gabeme@alleninstitute.org>
2019-12-09 12:50:39 -0800

    [FMS-872] Test that annotation does not double fetch

--- packages/fms-file-explorer-core/src/entity/Annotation/test/annotation.test.ts
+++ packages/fms-file-explorer-core/src/entity/Annotation/test/annotation.test.ts
@@ -1,17 +1,18 @@
-/* eslint-disable @typescript-eslint/camelcase */
 import { expect } from "chai";
+import { createSandbox, SinonSandbox, spy } from "sinon";
 
 import Annotation from "../";
+import AnnotationService from "../../../services/AnnotationService";
 
 describe("Annotation", () => {
-    describe("getDisplayValue", () => {
-        const annotationResponse = {
-            annotation_display_name: "Date created",
-            annotation_name: "created_on",
-            description: "Date the file was created on",
-            type: "date/time",
-        };
+    const annotationResponse = Object.freeze({
+        annotation_display_name: "Date created",
+        annotation_name: "created_on",
+        description: "Date the file was created on",
+        type: "date/time",
+    });
 
+    describe("getDisplayValue", () => {
         it("gets the display value for the annotation it represents from a given FmsFile", () => {
             const fmsFile = {
                 created_on: "2019-05-17T07:43:55.205Z",
@@ -31,6 +32,45 @@ describe("Annotation", () => {
 
             const annotation = new Annotation(annotationResponse);
             expect(annotation.getDisplayValue(fmsFile)).to.equal(Annotation.MISSING_VALUE);
+        });
+    });
+
+    describe("getValues", () => {
+        let sandbox: SinonSandbox;
+
+        before(() => {
+            sandbox = createSandbox();
+        });
+
+        afterEach(() => {
+            if (sandbox) {
+                sandbox.reset();
+            }
+        });
+
+        it("does not re-fetch annotation values if they are already loaded", async () => {
+            const annotationService = new AnnotationService();
+            const dates = [
+                new Date().toUTCString(),
+                new Date().toUTCString(),
+                new Date().toUTCString(),
+            ];
+
+            const fetchStub = sandbox
+                .stub(annotationService, "fetchValues")
+                .callsFake(() => Promise.resolve(dates));
+            const annotation = new Annotation(annotationResponse, annotationService);
+
+            // before
+            expect(fetchStub.callCount).to.equal(0);
+
+            // get values
+            expect(await annotation.getValues()).to.equal(dates);
+            expect(fetchStub.callCount).to.equal(1);
+
+            // get values again, still expect fetchStub to only have been called once
+            expect(await annotation.getValues()).to.equal(dates);
+            expect(fetchStub.callCount).to.equal(1);
         });
     });
 });
