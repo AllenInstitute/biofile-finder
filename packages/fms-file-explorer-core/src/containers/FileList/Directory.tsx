8e42950969bb73794af1496bfcc09e21bad29fc2
Gabe Medrash <gabeme@alleninstitute.org>
2019-12-18 09:51:08 -0800

    [FMS-879] Path data attribution

--- packages/fms-file-explorer-core/src/containers/FileList/Directory.tsx
+++ packages/fms-file-explorer-core/src/containers/FileList/Directory.tsx
@@ -25,12 +25,16 @@ interface DirectoryState {
 }
 
 /**
- * Path data for icons taken from Material Design
+ * Path data for icon taken from Material Design
  * Apache License 2.0 (https://github.com/google/material-design-icons/blob/master/LICENSE)
  */
 const FOLDER_ICON_PATH_DATA =
     "M9.984 3.984l2.016 2.016h8.016q0.797 0 1.383 0.609t0.586 1.406v9.984q0 0.797-0.586 1.406t-1.383 0.609h-16.031q-0.797 0-1.383-0.609t-0.586-1.406v-12q0-0.797 0.586-1.406t1.383-0.609h6z";
 
+/**
+ * Path data for icon designed by Daniel Bruce (www.entypo.com)
+ * License: https://creativecommons.org/licenses/by-sa/4.0/
+ */
 const CHEVRON_DOWN_ICON_PATH_DATA =
     "M13.418 7.859c0.271-0.268 0.709-0.268 0.978 0s0.272 0.701 0 0.969l-3.908 3.83c-0.27 0.268-0.707 0.268-0.979 0l-3.908-3.83c-0.27-0.267-0.27-0.701 0-0.969s0.709-0.268 0.978 0l3.421 3.141 3.418-3.141z";
 
