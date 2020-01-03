63aeea207e6ecca3ba73290fedd14fe4b291d0ac
Gabe Medrash <gabeme@alleninstitute.org>
2020-01-02 17:40:06 -0800

    [FMS-879] Clean up LazilyRenderedRow test

--- packages/fms-file-explorer-core/src/containers/FileList/test/LazilyRenderedRow.test.tsx
+++ packages/fms-file-explorer-core/src/containers/FileList/test/LazilyRenderedRow.test.tsx
@@ -1,4 +1,4 @@
-import { configureMockStore } from "@aics/redux-utils";
+import { configureMockStore, mergeState } from "@aics/redux-utils";
 import { expect } from "chai";
 import { mount } from "enzyme";
 import * as React from "react";
@@ -7,36 +7,37 @@ import * as sinon from "sinon";
 
 import Annotation from "../../../entity/Annotation";
 import { FmsFile } from "../../../services/FileService";
-import { ColumnWidths } from "../../DirectoryTree/useResizableColumns";
 import LazilyRenderedRow from "../LazilyRenderedRow";
 import { initialState } from "../../../state";
 
 describe("<LazilyRenderedRow />", () => {
+    const fileNameAnnotation = new Annotation({
+        annotation_display_name: "Name",
+        annotation_name: "file_name",
+        description: "name of file",
+        type: "string",
+    });
+
     function makeItemData() {
         const files = new Map<number, FmsFile>();
-        files.set(3, { file_id: "abc123", file_index: 3, file_name: "my_image.czi" }); // eslint-disable-line @typescript-eslint/camelcase
-
-        const rowWidth = 100;
+        files.set(3, { file_id: "abc123", file_name: "my_image.czi" });
 
         return {
-            columnWidths: new ColumnWidths(rowWidth, ["file_name"]),
-            displayAnnotations: [
-                new Annotation({
-                    annotation_display_name: "Name", // eslint-disable-line @typescript-eslint/camelcase
-                    annotation_name: "file_name", // eslint-disable-line @typescript-eslint/camelcase
-                    description: "name of file",
-                    type: "string",
-                }),
-            ],
-            level: 0,
             files,
             onSelect: sinon.spy(),
-            rowWidth,
         };
     }
 
-    it("renders data when it's available", () => {
-        const { store } = configureMockStore({ state: initialState });
+    it("renders data when available", () => {
+        const state = mergeState(initialState, {
+            metadata: {
+                annotations: [fileNameAnnotation],
+            },
+            selection: {
+                displayAnnotations: [fileNameAnnotation],
+            },
+        });
+        const { store } = configureMockStore({ state });
         const wrapper = mount(
             <Provider store={store}>
                 <LazilyRenderedRow data={makeItemData()} index={3} style={{}} />
@@ -48,7 +49,15 @@ describe("<LazilyRenderedRow />", () => {
     });
 
     it("renders a loading indicator when data is not available", () => {
-        const { store } = configureMockStore({ state: initialState });
+        const state = mergeState(initialState, {
+            metadata: {
+                annotations: [fileNameAnnotation],
+            },
+            selection: {
+                displayAnnotations: [fileNameAnnotation],
+            },
+        });
+        const { store } = configureMockStore({ state });
         const wrapper = mount(
             <Provider store={store}>
                 <LazilyRenderedRow data={makeItemData()} index={23} style={{}} />
