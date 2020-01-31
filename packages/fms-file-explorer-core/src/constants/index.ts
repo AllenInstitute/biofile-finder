6a1c18ed54a21b29ee3153673f82c42d8bcdc68a
Gabe Medrash <gabeme@alleninstitute.org>
2020-01-30 18:17:19 -0800

    [FMS-945] flat-file sentinel constant

--- packages/fms-file-explorer-core/src/constants/index.ts
+++ packages/fms-file-explorer-core/src/constants/index.ts
@@ -1,3 +1,6 @@
 export const APP_ID = "fms-file-explorer-core";
-export const API_VERSION = "v1";
-export const BASE_API_URL = `/fms-file-explorer-core/api/${API_VERSION}`;
+
+// This matches a special "host" value in fms-file-explorer-electron/src/main/menu
+// It is used as a sentinel to avoid making a call to the File Explorer Service when
+// the intention has been set to pull data from the flat files bundled in the Electron app.
+export const FLAT_FILE_DATA_SOURCE = "flat-file";
