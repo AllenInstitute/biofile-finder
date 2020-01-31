f611166f58a5bcf6a1b4b0d670311c02b9122ae9
Gabe Medrash <gabeme@alleninstitute.org>
2020-01-30 18:20:09 -0800

    [FMS-945] Pass FES cxn info to Annotation service

--- packages/fms-file-explorer-core/src/state/metadata/logics.ts
+++ packages/fms-file-explorer-core/src/state/metadata/logics.ts
@@ -1,16 +1,22 @@
 import { createLogic } from "redux-logic";
 
+import { ReduxLogicDeps } from "..";
 import { receiveAnnotations, REQUEST_ANNOTATIONS } from "./actions";
 import AnnotationService from "../../services/AnnotationService";
+import interaction from "../interaction";
 
 /**
  * Interceptor responsible for turning REQUEST_ANNOTATIONS action into a network call for available annotations. Outputs
  * RECEIVE_ANNOTATIONS actions.
  */
 const requestAnnotations = createLogic({
-    async process(deps, dispatch, done) {
+    async process(deps: ReduxLogicDeps, dispatch, done) {
+        const { getState } = deps;
+        const config = interaction.selectors.getFileExplorerServiceConnectionConfig(getState());
+        const annotationService = new AnnotationService(config);
+
         try {
-            dispatch(receiveAnnotations(await AnnotationService.fetchAnnotations()));
+            dispatch(receiveAnnotations(await annotationService.fetchAnnotations()));
         } catch (err) {
             console.error("Something went wrong, nobody knows why. But here's a hint:", err);
         } finally {
