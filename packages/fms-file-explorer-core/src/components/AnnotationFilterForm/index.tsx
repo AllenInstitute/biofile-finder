f548804ad6611a9a89b0f24357b6e82b8f9454d9
Gabe Medrash <gabeme@alleninstitute.org>
2020-02-14 16:45:47 -0800

    [FMS-771] Put a search box in the AnnotationFilterForm

--- packages/fms-file-explorer-core/src/components/AnnotationFilterForm/index.tsx
+++ packages/fms-file-explorer-core/src/components/AnnotationFilterForm/index.tsx
@@ -1,4 +1,5 @@
-import { List } from "office-ui-fabric-react";
+import * as Fuse from "fuse.js";
+import { List, SearchBox } from "office-ui-fabric-react";
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -13,12 +14,24 @@ interface AnnotationFilterFormProps {
     annotationName: string;
 }
 
+const FUZZY_SEARCH_OPTIONS = {
+    // which keys on ListItemData to search
+    keys: [{ name: "value", weight: 1 }],
+
+    // return resulting matches sorted
+    shouldSort: true,
+
+    // arbitrarily tuned; 0.0 requires a perfect match, 1.0 would match anything
+    threshold: 0.2,
+};
+
 export default function AnnotationFilterForm(props: AnnotationFilterFormProps) {
     const { annotationName } = props;
 
     const dispatch = useDispatch();
     const getAnnotationValueFilters = React.useMemo(makeFilterItemsSelector, []);
     const items = useSelector((state: State) => getAnnotationValueFilters(state, annotationName));
+    const [searchValue, setSearchValue] = React.useState("");
 
     const onFilterStateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.checked) {
@@ -34,12 +47,31 @@ export default function AnnotationFilterForm(props: AnnotationFilterFormProps) {
         }
     };
 
+    const onSearchBoxChange = (event?: React.ChangeEvent<HTMLInputElement>) => {
+        // bizzarely necessary because of typings on SearchBox
+        if (event) {
+            setSearchValue(event.target.value);
+        }
+    };
+
+    const filteredItems = React.useMemo(() => {
+        if (!searchValue) {
+            return items;
+        }
+
+        const fuse = new Fuse(items, FUZZY_SEARCH_OPTIONS);
+        return fuse.search(searchValue);
+    }, [items, searchValue]);
+
     return (
         <div className={styles.container} data-is-scrollable="true">
+            <div className={styles.header}>
+                <SearchBox className={styles.searchBox} onChange={onSearchBoxChange} />
+            </div>
             <List
                 getKey={(item) => String(item.value)}
-                items={items}
-                onShouldVirtualize={() => items.length > 100}
+                items={filteredItems}
+                onShouldVirtualize={() => filteredItems.length > 100}
                 onRenderCell={(item) =>
                     item && (
                         <FilterValue
