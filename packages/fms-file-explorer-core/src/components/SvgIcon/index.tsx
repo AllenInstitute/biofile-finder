537f678120dd3c592efd5cfe1938fd8cff8fc175
Gabe Medrash <gabeme@alleninstitute.org>
2020-02-19 19:15:40 -0800

    [FMS-771] Revert passing click event through SvgIcon for now

--- packages/fms-file-explorer-core/src/components/SvgIcon/index.tsx
+++ packages/fms-file-explorer-core/src/components/SvgIcon/index.tsx
@@ -7,7 +7,7 @@ const styles = require("./SvgIcon.module.css");
 interface SvgIconProps {
     className?: string;
     height?: number | string;
-    onClick?: (event: React.MouseEvent) => void;
+    onClick?: () => void;
     pathAttrs?: React.SVGProps<SVGPathElement>;
     pathData: string;
     width?: number | string;
