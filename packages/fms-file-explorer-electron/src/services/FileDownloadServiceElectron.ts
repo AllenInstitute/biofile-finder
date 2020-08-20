5196f21b5ce291b1e346b31b5cf3d104ff750bfb
seanm <seanm@alleninstitute.org>
2020-08-20 07:40:16 -0700

    Force a .csv file extension on manifest download

--- packages/fms-file-explorer-electron/src/services/FileDownloadServiceElectron.ts
+++ packages/fms-file-explorer-electron/src/services/FileDownloadServiceElectron.ts
@@ -28,6 +28,12 @@ export default class FileDownloadServiceElectron implements FileDownloadService 
         }
 
         return new Promise((resolve, reject) => {
+            // On Windows (at least) you have to self-append the file extension when overwriting the name
+            // I imagine this is not something a lot of people think to do (and is kind of inconvenient)
+            // - Sean M 08/20/20
+            const filePath = result.filePath.endsWith(".csv")
+                ? result.filePath
+                : result.filePath + ".csv";
             const req = http.request(
                 url,
                 {
@@ -52,13 +58,13 @@ export default class FileDownloadServiceElectron implements FileDownloadService 
                         });
                     } else {
                         res.on("end", () => {
-                            resolve(`CSV manifest saved to ${result.filePath}`);
+                            resolve(`CSV manifest saved to ${filePath}`);
                         });
                         res.on("error", (err) => {
                             reject(err.message);
                         });
 
-                        const writeStream = fs.createWriteStream(result.filePath as string);
+                        const writeStream = fs.createWriteStream(filePath);
                         res.pipe(writeStream);
                     }
                 }
