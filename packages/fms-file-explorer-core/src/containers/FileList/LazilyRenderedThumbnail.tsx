2177d96e59f3ce922776b371a7c53c965d9d3f61
Gabe Medrash <gabeme@alleninstitute.org>
2020-09-10 21:09:34 -0700

    [WIP] Add icon attribution

--- packages/fms-file-explorer-core/src/containers/FileList/LazilyRenderedThumbnail.tsx
+++ packages/fms-file-explorer-core/src/containers/FileList/LazilyRenderedThumbnail.tsx
@@ -29,8 +29,8 @@ interface LazilyRenderedThumbnailProps {
 }
 
 /**
- * Path data for icon taken from Material Design
- * Apache License 2.0: https://github.com/google/material-design-icons/blob/master/LICENSE
+ * Path data for icon taken from Hawcons
+ * Copyright Yannick Lung, permissively licensed: http://hawcons.com/
  */
 const FILE_ICON_PATH_DATA =
     "M19.5 3h0.5l6 7v18.009c0 1.093-0.894 1.991-1.997 1.991h-15.005c-1.107 0-1.997-0.899-1.997-2.007v-22.985c0-1.109 0.897-2.007 2.003-2.007h10.497zM19 4h-10.004c-0.55 0-0.996 0.455-0.996 0.995v23.009c0 0.55 0.455 0.995 1 0.995h15c0.552 0 1-0.445 1-0.993v-17.007h-4.002c-1.103 0-1.998-0.887-1.998-2.006v-4.994zM20 4.5v4.491c0 0.557 0.451 1.009 0.997 1.009h3.703l-4.7-5.5z";
