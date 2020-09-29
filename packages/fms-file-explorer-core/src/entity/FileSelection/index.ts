1720ff279c4405b0ee6ea85bdad67456ca3edd89
Gabe Medrash <gabeme@alleninstitute.org>
2020-09-29 13:54:02 -0700

    [FMS-1367] Use FileSet::equals to compare FileSets

--- packages/fms-file-explorer-core/src/entity/FileSelection/index.ts
+++ packages/fms-file-explorer-core/src/entity/FileSelection/index.ts
@@ -135,7 +135,7 @@ export default class FileSelection {
 
         // if `indexRange` contains already selected file rows, compact
         const compacted = reject(this.selections, (existingSelectionItem) => {
-            return existingSelectionItem.fileSet === fileSet && indexRange.contains(existingSelectionItem.selection);
+            return existingSelectionItem.fileSet.equals(fileSet) && indexRange.contains(existingSelectionItem.selection);
         });
 
         const item: SelectionItem = {
