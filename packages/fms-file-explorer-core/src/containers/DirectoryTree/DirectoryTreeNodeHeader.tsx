7155b7eacdb1518782f7bfcf3cbe2ceb727115dd
seanm <seanm@alleninstitute.org>
2020-10-16 17:04:34 -0700

    Add error icon & copy button

--- packages/fms-file-explorer-core/src/containers/DirectoryTree/DirectoryTreeNodeHeader.tsx
+++ packages/fms-file-explorer-core/src/containers/DirectoryTree/DirectoryTreeNodeHeader.tsx
@@ -41,7 +41,7 @@ const CHEVRON_DOWN_ICON_PATH_DATA =
  * Path data for icon taken from Material Design
  * Apache License 2.0: https://github.com/google/material-design-icons/blob/master/LICENSE
  */
-const ERROR_ICON_PATH_DATA =
+export const ERROR_ICON_PATH_DATA =
     "M12 20.016q3.281 0 5.648-2.367t2.367-5.648-2.367-5.648-5.648-2.367-5.648 2.367-2.367 5.648 2.367 5.648 5.648 2.367zM12 2.016q4.125 0 7.055 2.93t2.93 7.055-2.93 7.055-7.055 2.93-7.055-2.93-2.93-7.055 2.93-7.055 7.055-2.93zM11.016 6.984h1.969v6h-1.969v-6zM11.016 15h1.969v2.016h-1.969v-2.016z";
 
 const ICON_SIZE = 15; // in px; both width and height
