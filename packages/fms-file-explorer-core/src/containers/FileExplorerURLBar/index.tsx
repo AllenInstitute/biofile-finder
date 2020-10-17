afc1d694593ae57a3e7db80648c662d7cc665a47
seanm <seanm@alleninstitute.org>
2020-10-16 17:04:34 -0700

    Add error icon & copy button

--- packages/fms-file-explorer-core/src/containers/FileExplorerURLBar/index.tsx
+++ packages/fms-file-explorer-core/src/containers/FileExplorerURLBar/index.tsx
@@ -1,8 +1,11 @@
+import Tippy from "@tippy.js/react";
 import classNames from "classnames";
-import { IconButton, TextField } from "office-ui-fabric-react";
+import { IconButton, TextField, TooltipHost } from "office-ui-fabric-react";
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import { ERROR_ICON_PATH_DATA } from "../DirectoryTree/DirectoryTreeNodeHeader";
+import SvgIcon from "../../components/SvgIcon";
 import FileExplorerURL from "../../entity/FileExplorerURL";
 import { metadata, selection } from "../../state";
 
@@ -13,71 +16,72 @@ interface FileExplorerURLBarProps {
 }
 
 /**
- * TODO
+ * URL bar at the top of the application responsible for allowing the copy & paste
+ * of FileExplorerURLs for the purpose of sharing queries between users.
  */
 export default function FileExplorerURLBar(props: FileExplorerURLBarProps) {
     const dispatch = useDispatch();
     const annotations = useSelector(metadata.selectors.getAnnotations);
     const existingEncodedURL = useSelector(selection.selectors.getEncodedFileExplorerUrl);
 
-    const [url, setURL] = React.useState<string>();
+    const [url, setURL] = React.useState<string>(existingEncodedURL);
+    const [isCopied, setCopied] = React.useState(false);
+    const error = React.useMemo(
+        () => FileExplorerURL.validateEncodedFileExplorerURL(url, annotations),
+        [url, annotations]
+    );
 
     // Update URL bar (even if input exists) if the state changes enough to change what would be encoded there
     React.useEffect(() => {
         setURL(existingEncodedURL);
     }, [existingEncodedURL]);
 
-    // TODO: On enter
-    const decodeInput = () => {
-        if (url && url !== existingEncodedURL) {
-            console.log("Dispatching url" + url);
+    const onCopy = () => {
+        setCopied(true);
+        setTimeout(() => {
+            setCopied(false);
+        }, 4000);
+        navigator.clipboard.writeText(url || existingEncodedURL);
+    };
+
+    const onSubmit = (evt: React.FormEvent) => {
+        evt.preventDefault();
+        if (url !== existingEncodedURL && !error) {
             dispatch(selection.actions.decodeFileExplorerURL(url));
         }
     };
 
-    const validateURL = (encodedURL: string) => {
-        const errorMessage = FileExplorerURL.validateEncodedFileExplorerURL(
-            encodedURL,
-            annotations
-        );
-        if (errorMessage) {
-            return `This could not be verified as a File Explorer URL. ${errorMessage}`;
-        }
-        return "";
-    };
-    const validationErrorMessage = url ? validateURL(url) : undefined;
-
-    const copyUrl = () => {
-        // TODO: Tell user when it has copied to their clipboard
-        navigator.clipboard.writeText(url || existingEncodedURL);
-    };
-
     return (
-        <div
-            className={classNames(
-                styles.container,
-                {
-                    [styles.containerWithError]: validationErrorMessage,
-                },
-                props.className
+        <div className={classNames(styles.container, props.className)}>
+            <form className={styles.urlForm} onSubmit={onSubmit}>
+                <TextField
+                    borderless
+                    prefix={"FMS File Explorer URL"}
+                    defaultValue={existingEncodedURL}
+                    onChange={(_, value) => setURL(value || "")}
+                    value={url}
+                    spellCheck={false}
+                />
+            </form>
+            {!error ? (
+                <TooltipHost content={isCopied ? "Copied to clipboard!" : undefined}>
+                    <IconButton
+                        className={styles.copyButton}
+                        iconProps={{ iconName: "link" }}
+                        onClick={onCopy}
+                    />
+                </TooltipHost>
+            ) : (
+                <Tippy content={`This could not be verified as a File Explorer URL. ${error}`}>
+                    <SvgIcon
+                        className={styles.errorIcon}
+                        height={15}
+                        pathData={ERROR_ICON_PATH_DATA}
+                        viewBox="0 0 24 24"
+                        width={15}
+                    />
+                </Tippy>
             )}
-        >
-            <TextField
-                borderless
-                className={styles.url}
-                errorMessage={validationErrorMessage}
-                prefix={"FMS File Explorer URL"}
-                defaultValue={existingEncodedURL}
-                onChange={(_, value) => setURL(value)}
-                value={url}
-                spellCheck={false}
-            />
-            <IconButton
-                className={styles.link}
-                disabled={Boolean(validationErrorMessage)}
-                iconProps={{ iconName: "link" }}
-                onClick={copyUrl}
-            />
         </div>
     );
 }
