25880c5564fce85c7bee4714a0730502c8e411c6
seanm <seanm@alleninstitute.org>
2020-10-26 15:58:19 -0700

    Test adjusted selector logic

--- packages/fms-file-explorer-core/src/containers/AnnotationSidebar/test/selectors.test.ts
+++ packages/fms-file-explorer-core/src/containers/AnnotationSidebar/test/selectors.test.ts
@@ -5,6 +5,7 @@ import Annotation from "../../../entity/Annotation";
 import { annotationsJson } from "../../../entity/Annotation/mocks";
 import * as annotationSelectors from "../selectors";
 import { initialState } from "../../../state";
+import FileFilter from "../../../entity/FileFilter";
 
 type PartialDeep<T> = {
     [P in keyof T]?: PartialDeep<T[P]>;
@@ -35,44 +36,71 @@ describe("<AnnotationSidebar /> selectors", () => {
             expect(first).to.have.property("description", "AICS cell line");
             expect(first).to.have.property("title", "Cell line");
         });
-    });
 
-    describe("getNonAvailableAnnotationsForHierarchy", () => {
-        it("filters annotation list items to exclude available annotations", () => {
+        it("denotes filtered annotations as filtered", () => {
+            const filters = [
+                new FileFilter("Cell Line", "AICS-0"),
+                new FileFilter("Date Created", "01/10/15"),
+            ];
             const state = mergeState(initialState, {
                 metadata: {
                     annotations: map(annotationsJson, (annotation) => new Annotation(annotation)),
                 },
                 selection: {
-                    annotationHierarchy: [
-                        new Annotation({
-                            annotationName: "color",
-                            annotationDisplayName: "Color",
-                            description: "a color",
-                            type: "text",
-                        }),
-                    ],
-                    availableAnnotationsForHierarchy: ["date_created", "cell_dead"],
+                    filters,
                 },
             });
 
-            const listItems = annotationSelectors.getNonAvailableAnnotationsForHierarchy(state);
-            expect(listItems.length).to.equal(annotationsJson.length - 2);
+            const listItems = annotationSelectors.getAnnotationListItems(state);
+            expect(listItems.length).to.equal(annotationsJson.length);
+
+            listItems.forEach((item) => {
+                const filtered = filters.findIndex((f) => f.name === item.id) !== -1;
+                expect(item).to.have.property("filtered", filtered);
+            });
         });
 
-        it("considers all annotations viable when no hierarchy is present", () => {
+        it("denotes non-available annotations as disabled", () => {
+            const annotations = map(annotationsJson, (annotation) => new Annotation(annotation));
+            const availableAnnotationsForHierarchy = annotations.slice(1, 3).map((a) => a.name);
+            const availableAnnotationsForHierarchySet = new Set(availableAnnotationsForHierarchy);
             const state = mergeState(initialState, {
                 metadata: {
-                    annotations: map(annotationsJson, (annotation) => new Annotation(annotation)),
+                    annotations,
                 },
                 selection: {
-                    annotationHierarchy: [],
-                    availableAnnotationsForHierarchy: ["date_created", "cell_dead"],
+                    availableAnnotationsForHierarchy,
+                    annotationHierarchy: annotations.slice(1, 2),
                 },
             });
 
-            const listItems = annotationSelectors.getNonAvailableAnnotationsForHierarchy(state);
-            expect(listItems).to.be.empty;
+            const listItems = annotationSelectors.getAnnotationListItems(state);
+            expect(listItems.length).to.equal(annotationsJson.length);
+
+            listItems.forEach((item) => {
+                const disabled = !availableAnnotationsForHierarchySet.has(item.id);
+                expect(item).to.have.property("disabled", disabled);
+            });
+        });
+
+        it("denotes all annotations as enabled when hierarchy is empty", () => {
+            const annotations = map(annotationsJson, (annotation) => new Annotation(annotation));
+            const availableAnnotationsForHierarchy = annotations.slice(1, 3).map((a) => a.name);
+            const state = mergeState(initialState, {
+                metadata: {
+                    annotations,
+                },
+                selection: {
+                    availableAnnotationsForHierarchy,
+                },
+            });
+
+            const listItems = annotationSelectors.getAnnotationListItems(state);
+            expect(listItems.length).to.equal(annotationsJson.length);
+
+            listItems.forEach((item) => {
+                expect(item).to.have.property("disabled", false);
+            });
         });
     });
 });
