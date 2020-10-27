b8906c8e487c3634f5337d6aa07aa52b888cb995
Gabe Medrash <gabeme@alleninstitute.org>
2020-10-26 21:03:39 -0700

    [FMS-1392] Only show medallion as clickable if clicking it does something

--- packages/fms-file-explorer-core/src/containers/FilterDisplayBar/FilterMedallion.tsx
+++ packages/fms-file-explorer-core/src/containers/FilterDisplayBar/FilterMedallion.tsx
@@ -32,23 +32,46 @@ const BUTTON_STYLES = {
 };
 
 /**
- * UI for displaying the annotation values applies as file filters for a single annotation.
- * Each `FileFilter` within `props.filters` must related to the same annotation (e.g. `FileFilter.name`
- * for each should be equal).
+ * UI for displaying the annotation values applied as file filters. Each `FileFilter` within `props.filters`
+ * must relate to the same annotation (e.g. Each `FileFilter::name` should be equal).
  */
 export default function FilterMedallion(props: Props) {
     const { filters, name } = props;
-    const [expand, setExpand] = React.useState(false);
     const dispatch = useDispatch();
+
+    const [expanded, setExpanded] = React.useState(false);
+    const [overflowing, setOverflowing] = React.useState(false);
+    const ref = React.useRef<HTMLElement | null>(null);
+
+    // Determine if medallion has reached its max-width and text is overflowing
+    // If a medallion is no longer overflowing but its "expanded" state is truthy, reset.
+    React.useEffect(() => {
+        if (ref.current) {
+            const width = ref.current.clientWidth;
+            const scrollWidth = ref.current.scrollWidth;
+            const isOverflowing = scrollWidth > width;
+            setOverflowing(isOverflowing);
+            if (!isOverflowing) {
+                setExpanded(false);
+            }
+        } else {
+            setOverflowing(false);
+        }
+    }, [ref, filters]);
 
     const operator = filters.length > 1 ? "ONE OF" : "EQUALS";
     const valueDisplay = map(filters, (filter) => filter.value).join(", ");
     const display = `${name} ${operator} ${valueDisplay}`;
+
     return (
         <div className={styles.medallion}>
             <abbr
-                className={classNames(styles.medallionText, { [styles.expanded]: expand })}
-                onClick={() => setExpand((prev) => !prev)}
+                className={classNames(styles.medallionText, {
+                    [styles.expandable]: overflowing,
+                    [styles.expanded]: expanded,
+                })}
+                onClick={() => overflowing && setExpanded((prev) => !prev)}
+                ref={ref}
                 title={display}
             >
                 {display}
