eb53475f1a11c787b9c62026f8cfd58b4082ac9d
seanm <seanm@alleninstitute.org>
2020-11-17 09:27:11 -0800

    Add tests for python snippet modal

---
+++ packages/fms-file-explorer-core/src/containers/DialogModal/PythonSnippet/test/PythonSnippet.test.tsx
@@ -0,0 +1,49 @@
+import { configureMockStore, mergeState } from "@aics/redux-utils";
+import { render } from "@testing-library/react";
+import { expect } from "chai";
+import * as React from "react";
+import { Provider } from "react-redux";
+
+import DialogModal, { Modal } from "../..";
+import { initialState } from "../../../../state";
+
+describe("<PythonSnippet />", () => {
+    const visibleDialogState = mergeState(initialState, {
+        interaction: {
+            visibleModal: Modal.PythonSnippet,
+        },
+    });
+
+    it("is visible when should not be hidden", async () => {
+        // Arrange
+        const { store } = configureMockStore({ state: visibleDialogState });
+        const { getByText } = render(
+            <Provider store={store}>
+                <DialogModal />
+            </Provider>
+        );
+
+        // Assert
+        expect(getByText("Setup")).to.exist;
+        expect(getByText("Code")).to.exist;
+    });
+
+    it("displays snippet when present in state", async () => {
+        // Arrange
+        const pythonSnippet = "data = MyPython.Query('Cell Line')";
+        const state = mergeState(visibleDialogState, {
+            interaction: {
+                pythonSnippet,
+            },
+        });
+        const { store } = configureMockStore({ state });
+        const { findByText } = render(
+            <Provider store={store}>
+                <DialogModal />
+            </Provider>
+        );
+
+        // Assert
+        expect(await findByText(pythonSnippet)).to.exist;
+    });
+});
