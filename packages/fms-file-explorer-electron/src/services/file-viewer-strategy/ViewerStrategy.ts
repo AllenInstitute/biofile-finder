984c39671f8e530f6ae8456a4b2e26ace6085aef
Gabe Medrash <gabeme@alleninstitute.org>
2021-01-14 11:02:06 -0800

    [FMS-1409] Docstring for ViewerStrategy interface

--- packages/fms-file-explorer-electron/src/services/file-viewer-strategy/ViewerStrategy.ts
+++ packages/fms-file-explorer-electron/src/services/file-viewer-strategy/ViewerStrategy.ts
@@ -1,3 +1,8 @@
+/**
+ * Interface for concrete viewer strategies to implement. A viewer strategy defines
+ * an interchangeable function for opening a file viewer given a path to
+ * the file viewer's executable and a list of file paths to open.
+ */
 type ViewerStrategy = (executable: string, filePaths: string[]) => Promise<void>;
 
 export default ViewerStrategy;
