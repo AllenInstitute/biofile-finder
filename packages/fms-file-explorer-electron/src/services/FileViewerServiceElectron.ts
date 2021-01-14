968aef72225f7ac5d8a3ec88c9fe55855b6e6889
Gabe Medrash <gabeme@alleninstitute.org>
2021-01-14 10:48:37 -0800

    [FMS-1409] Delegate to proper ViewerStrategy

--- packages/fms-file-explorer-electron/src/services/FileViewerServiceElectron.ts
+++ packages/fms-file-explorer-electron/src/services/FileViewerServiceElectron.ts
@@ -1,8 +1,9 @@
-import childProcess from "child_process";
-
 import { FileViewerService } from "@aics/fms-file-explorer-core";
 
 import NotificationServiceElectron from "./NotificationServiceElectron";
+import defaultViewerStrategy from "./file-viewer-strategy/defaultViewerStrategy";
+import fijiViewerStrategy from "./file-viewer-strategy/fijiViewerStrategy";
+import ViewerStrategy from "./file-viewer-strategy/ViewerStrategy";
 
 export default class FileViewerServiceElectron implements FileViewerService {
     private notificationService: NotificationServiceElectron;
@@ -19,16 +20,19 @@ export default class FileViewerServiceElectron implements FileViewerService {
             );
         };
 
+        // N.b.: this is a purposeful kludge of the strategy pattern: instead of keeping the context (this class)
+        // unaware of concrete strategies, encode some heuristics about which concrete strategy to use.
+        // This simplifies the usage of this class; at runtime, the explorer-core package can continue to
+        // remain ignorant of the low-level details required to programmatically use any particular executable,
+        // and opens the possibility for removing the need for the explorer-core to know anything at all about
+        // the executable chosen.
+        let viewerStrategy: ViewerStrategy = defaultViewerStrategy;
+        if (executable.includes("Fiji") || RegExp("ImageJ-.*$").test(executable)) {
+            viewerStrategy = fijiViewerStrategy;
+        }
+
         try {
-            // Create a child process for the executable to open files in
-            const executableProcess = childProcess.spawn(executable, filePaths);
-            // Handle unsuccessful startups of the executable (these will only be called if explorer is still open)
-            executableProcess.on("error", reportErrorToUser);
-            executableProcess.on("exit", async (code: number) => {
-                if (code !== 0) {
-                    await reportErrorToUser(`Status Code ${code}`);
-                }
-            });
+            await viewerStrategy(executable, filePaths);
         } catch (error) {
             await reportErrorToUser(error);
         }
