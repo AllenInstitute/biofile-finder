3ae97ca92beffb82ef46105d58b9f2cb3d6c0cf0
Gabe Medrash <gabeme@alleninstitute.org>
2021-02-03 18:18:03 -0800

    [FMS-1384] Make use of .env to avoid including any API keys in git

--- packages/fms-file-explorer-electron/webpack/plugins.js
+++ packages/fms-file-explorer-electron/webpack/plugins.js
@@ -1,49 +1,53 @@
-const path = require('path');
+const path = require("path");
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-const Env = require('./constants').Env;
-const devServer = require('./constants').devServer;
+const Env = require("./constants").Env;
+const devServer = require("./constants").devServer;
+const packageJson = require("../package.json");
 
 const BASE_PLUGINS = [
     new ForkTsCheckerWebpackPlugin({
-        tsconfig: path.resolve(__dirname, '../', 'tsconfig.json'),
+        tsconfig: path.resolve(__dirname, "../", "tsconfig.json"),
         workers: ForkTsCheckerWebpackPlugin.ONE_CPU,
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
+    new MiniCssExtractPlugin({ filename: "style.[contenthash].css" }),
     new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'index.html')
-    })
+        template: path.resolve(__dirname, "index.html"),
+    }),
+    new Dotenv({
+        systemvars: true, // load all system variables in addition to what we find in .env
+    }),
 ];
 
-const BUNDLE_ANALYZER = [new BundleAnalyzerPlugin({ analyzerMode: 'static' })];
+const BUNDLE_ANALYZER = [new BundleAnalyzerPlugin({ analyzerMode: "static" })];
 
 const PLUGINS_BY_ENV = {
     [Env.PRODUCTION]: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production'),
+            "process.env.APPLICATION_VERSION": JSON.stringify(packageJson.version),
+            "process.env.NODE_ENV": JSON.stringify("production"),
         }),
-        new webpack.HashedModuleIdsPlugin()
-    ],
-    [Env.STAGE]: [
-        new webpack.NamedModulesPlugin()
+        new webpack.HashedModuleIdsPlugin(),
     ],
     [Env.DEVELOPMENT]: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development'),
-            'process.env.WEBPACK_DEV_SERVER_PORT': JSON.stringify(devServer.port),
+            "process.env.APPLICATION_VERSION": JSON.stringify(packageJson.version),
+            "process.env.NODE_ENV": JSON.stringify("development"),
+            "process.env.WEBPACK_DEV_SERVER_PORT": JSON.stringify(devServer.port),
         }),
-    ]
+    ],
 };
 
 module.exports = (env, analyzer) => [
     ...BASE_PLUGINS,
     ...(analyzer ? BUNDLE_ANALYZER : []),
-    ...(PLUGINS_BY_ENV[env] || [])
+    ...(PLUGINS_BY_ENV[env] || []),
 ];
--- packages/fms-file-explorer-web/webpack/plugins.js
+++ packages/fms-file-explorer-web/webpack/plugins.js
@@ -1,42 +1,51 @@
-const path = require('path');
+const path = require("path");
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-const Env = require('./constants').Env;
+const Env = require("./constants").Env;
+const packageJson = require("../package.json");
 
 const BASE_PLUGINS = [
     new ForkTsCheckerWebpackPlugin({
-        tsconfig: path.resolve(__dirname, '../', 'tsconfig.json'),
+        tsconfig: path.resolve(__dirname, "../", "tsconfig.json"),
         workers: ForkTsCheckerWebpackPlugin.ONE_CPU,
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
+    new MiniCssExtractPlugin({ filename: "style.[contenthash].css" }),
     new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'index.html')
-    })
+        template: path.resolve(__dirname, "index.html"),
+    }),
+    new Dotenv({
+        systemvars: true, // load all system variables in addition to what we find in .env
+    }),
 ];
 
-const BUNDLE_ANALYZER = [new BundleAnalyzerPlugin({ analyzerMode: 'static' })];
+const BUNDLE_ANALYZER = [new BundleAnalyzerPlugin({ analyzerMode: "static" })];
 
 const PLUGINS_BY_ENV = {
     [Env.PRODUCTION]: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production'),
+            "process.env.APPLICATION_VERSION": JSON.stringify(packageJson.version),
+            "process.env.NODE_ENV": JSON.stringify("production"),
         }),
-        new webpack.HashedModuleIdsPlugin()
+        new webpack.HashedModuleIdsPlugin(),
     ],
-    [Env.STAGE]: [
-        new webpack.NamedModulesPlugin()
-    ]
+    [Env.DEVELOPMENT]: [
+        new webpack.DefinePlugin({
+            "process.env.APPLICATION_VERSION": JSON.stringify(packageJson.version),
+            "process.env.NODE_ENV": JSON.stringify("development"),
+        }),
+    ],
 };
 
 module.exports = (env, analyzer) => [
     ...BASE_PLUGINS,
     ...(analyzer ? BUNDLE_ANALYZER : []),
-    ...(PLUGINS_BY_ENV[env] || [])
+    ...(PLUGINS_BY_ENV[env] || []),
 ];
