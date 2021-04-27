2a4516e2bd54514b32642f1fac15e0ff0fc7ca5f
seanm <seanm@alleninstitute.org>
2021-04-27 16:41:45 -0700

    Test Windows CWD option

--- packages/desktop/src/services/file-viewer-strategy/systemDefaultViewerStrategy.ts
+++ packages/desktop/src/services/file-viewer-strategy/systemDefaultViewerStrategy.ts
@@ -1,30 +1,41 @@
 import * as os from "os";
+import * as path from "path";
 
 import childProcess from "child_process";
 import { Platform } from "../ExecutionEnvServiceElectron";
 
 import ViewerStrategy from "./ViewerStrategy";
 
+const PROGRAMS_FOLDER_SEP = `${path.sep}programs${path.sep}allencell${path.sep}`;
+
 /**
  * Spawn executable with default system command for opening files with file paths as arguments.
  */
 const systemDefaultViewerStrategy: ViewerStrategy = async (_, filePaths) => {
     // Determine the default system command for opening files (default Linux)
+    let cwd: string | undefined;
+    let shell: string | boolean = false;
     let command = "gio";
     let args = ["open", ...filePaths];
     if (os.platform() === Platform.Mac) {
         command = "open";
         args = filePaths;
     } else if (os.platform() === Platform.Windows) {
+        shell = true;
         command = "start";
+        // Set current working directory to inside allen drive to avoid
+        // weirdness with the "start" command and UNC paths
+        cwd = `${filePaths[0].split(PROGRAMS_FOLDER_SEP)[0]}${PROGRAMS_FOLDER_SEP}`;
+        const files = filePaths.map((filePath) => filePath.split(PROGRAMS_FOLDER_SEP)[1]);
         // The "start" command does not accept multiple file arguments so
         // this chains the commands together instead
-        args = filePaths.join(" && start ").split(" ");
+        args = files.join(" ; start ").split(" ");
     }
 
     const executableProcess = childProcess.spawn(command, args, {
+        cwd,
+        shell,
         detached: true,
-        shell: os.platform() === Platform.Windows,
         stdio: "ignore", // If the parent's stdio is inherited, the child will remain attached to the controlling terminal.
     });
 
