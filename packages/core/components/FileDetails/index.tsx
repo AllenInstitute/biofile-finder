797377c087493b7bf5dc71f5e0a86fdb51ae5468
tylerf <tylerf@alleninstitute.org>
2021-09-03 13:27:12 -0700

    [UR-2] Move resizeHandle function definitions outside the component function since they don't technically need to be there

--- packages/core/components/FileDetails/index.tsx
+++ packages/core/components/FileDetails/index.tsx
@@ -27,6 +27,50 @@ export const WINDOW_ACTION_BUTTON_WIDTH = 23; // arbitrary
 const FILE_DETAILS_PANE_ID = "file-details-pane";
 const FILE_DETAILS_WIDTH_ATTRIBUTE = "--file-details-width";
 
+function resizeHandleOnMouseDown(mouseDownEvent: React.MouseEvent<HTMLDivElement>) {
+    const rootElement: HTMLElement | null = document.getElementById(ROOT_ELEMENT_ID);
+    const fileDetailsPane: HTMLElement | null = document.getElementById(FILE_DETAILS_PANE_ID);
+    // This _should_ always be true, but is otherwise not a huge deal
+    if (rootElement && fileDetailsPane) {
+        fileDetailsPane.classList.remove(styles.expandableTransition);
+
+        const startingWidth = fileDetailsPane.offsetWidth;
+        const startingPageX = mouseDownEvent.pageX;
+
+        const resizeHandleRootOnMouseMove = (mouseMoveEvent: MouseEvent) => {
+            mouseMoveEvent.preventDefault();
+            const newWidth = startingWidth + (startingPageX - mouseMoveEvent.pageX);
+
+            if (mouseMoveEvent.buttons === 1 && newWidth >= 175) {
+                // If primary button (left-click) is still pressed and newWidth is still greater
+                //   than the minimum width we want for the pane
+                rootElement.style.setProperty(FILE_DETAILS_WIDTH_ATTRIBUTE, `${newWidth}px`);
+            } else {
+                // Remove this listener if user releases the primary button
+                rootElement.removeEventListener("mousemove", resizeHandleRootOnMouseMove);
+                fileDetailsPane.classList.add(styles.expandableTransition);
+            }
+        };
+
+        rootElement.addEventListener("mousemove", resizeHandleRootOnMouseMove);
+    } else {
+        console.log(`"${ROOT_ELEMENT_ID}" element or "${FILE_DETAILS_PANE_ID}" element not found`);
+    }
+}
+
+function resizeHandleDoubleClick() {
+    const rootElement: HTMLElement | null = document.getElementById(ROOT_ELEMENT_ID);
+    const fileDetailsPane: HTMLElement | null = document.getElementById(FILE_DETAILS_PANE_ID);
+
+    if (rootElement && fileDetailsPane) {
+        // Return details pane width to the default (20%)
+        fileDetailsPane.classList.add(styles.expandableTransition);
+        rootElement.style.setProperty(FILE_DETAILS_WIDTH_ATTRIBUTE, "20%");
+    } else {
+        console.log(`"${ROOT_ELEMENT_ID}" element or "${FILE_DETAILS_PANE_ID}" element not found`);
+    }
+}
+
 /**
  * Right-hand sidebar of application. Displays details of selected file(s).
  */
@@ -51,54 +95,6 @@ export default function FileDetails(props: FileDetails) {
             fileDetails?.name.toLowerCase().endsWith(format)
         );
         if (isFileRenderableImage) thumbnailUriPath = fileDetails.path;
-    }
-
-    function resizeHandleOnMouseDown(mouseDownEvent: React.MouseEvent<HTMLDivElement>) {
-        const rootElement: HTMLElement | null = document.getElementById(ROOT_ELEMENT_ID);
-        const fileDetailsPane: HTMLElement | null = document.getElementById(FILE_DETAILS_PANE_ID);
-        // This _should_ always be true, but is otherwise not a huge deal
-        if (rootElement && fileDetailsPane) {
-            fileDetailsPane.classList.remove(styles.expandableTransition);
-
-            const startingWidth = fileDetailsPane.offsetWidth;
-            const startingPageX = mouseDownEvent.pageX;
-
-            const resizeHandleRootOnMouseMove = (mouseMoveEvent: MouseEvent) => {
-                mouseMoveEvent.preventDefault();
-                const newWidth = startingWidth + (startingPageX - mouseMoveEvent.pageX);
-
-                if (mouseMoveEvent.buttons === 1 && newWidth >= 175) {
-                    // If primary button (left-click) is still pressed and newWidth is still greater
-                    //   than the minimum width we want for the pane
-                    rootElement.style.setProperty(FILE_DETAILS_WIDTH_ATTRIBUTE, `${newWidth}px`);
-                } else {
-                    // Remove this listener if user releases the primary button
-                    rootElement.removeEventListener("mousemove", resizeHandleRootOnMouseMove);
-                    fileDetailsPane.classList.add(styles.expandableTransition);
-                }
-            };
-
-            rootElement.addEventListener("mousemove", resizeHandleRootOnMouseMove);
-        } else {
-            console.log(
-                `"${ROOT_ELEMENT_ID}" element or "${FILE_DETAILS_PANE_ID}" element not found`
-            );
-        }
-    }
-
-    function resizeHandleDoubleClick() {
-        const rootElement: HTMLElement | null = document.getElementById(ROOT_ELEMENT_ID);
-        const fileDetailsPane: HTMLElement | null = document.getElementById(FILE_DETAILS_PANE_ID);
-
-        if (rootElement && fileDetailsPane) {
-            // Return details pane width to the default (20%)
-            fileDetailsPane.classList.add(styles.expandableTransition);
-            rootElement.style.setProperty(FILE_DETAILS_WIDTH_ATTRIBUTE, "20%");
-        } else {
-            console.log(
-                `"${ROOT_ELEMENT_ID}" element or "${FILE_DETAILS_PANE_ID}" element not found`
-            );
-        }
     }
 
     return (
