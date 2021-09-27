d3cc581ba29557ea9c552e178e1faf61ce16466a
SeanLeRoy <seanm@alleninstitute.org>
2021-09-27 10:04:24 -0700

    Fix action request

--- packages/core/App.tsx
+++ packages/core/App.tsx
@@ -66,7 +66,7 @@ export default function App(props: AppProps) {
         batch(() => {
             dispatch(interaction.actions.setFileExplorerServiceBaseUrl(fileExplorerServiceBaseUrl));
             dispatch(metadata.actions.requestAnnotations());
-            dispatch(metadata.actions.requestDatasets());
+            dispatch(metadata.actions.requestCollections());
             dispatch(selection.actions.setAnnotationHierarchy([]));
         });
     }, [dispatch, fileExplorerServiceBaseUrl]);
