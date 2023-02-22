e2f8dbd0220dbb44c56eef5887493126bd55db28
SeanLeRoy <seanme97@gmail.com>
2023-02-22 14:08:50 -0800

    [SWE-735] Find better target for file-list

--- packages/core/components/FileList/index.tsx
+++ packages/core/components/FileList/index.tsx
@@ -8,7 +8,6 @@ import { FixedSizeList } from "react-window";
 import InfiniteLoader from "react-window-infinite-loader";
 
 import FileSet from "../../entity/FileSet";
-import Tutorial from "../../entity/Tutorial";
 import Header from "./Header";
 import LazilyRenderedRow from "./LazilyRenderedRow";
 import { selection } from "../../state";
@@ -181,7 +180,6 @@ export default function FileList(props: FileListProps) {
                 style={{
                     height: isRoot ? undefined : `${calculatedHeight}px`,
                 }}
-                id={Tutorial.FILE_LIST_ID}
                 ref={measuredNodeRef}
             >
                 {content}
