0b411a367e1e160e56602ee1f1e5466ef5ceb085
Brian Kim <brian.kim@alleninstitute.org>
2023-04-27 09:36:39 -0700

    oops pt 2

--- packages/core/components/FileList/useFileAccessContextMenu.ts
+++ packages/core/components/FileList/useFileAccessContextMenu.ts
@@ -5,7 +5,6 @@ import FileFilter from "../../entity/FileFilter";
 import { interaction, selection } from "../../state";
 import { ContextMenuItem } from "../ContextMenu";
 import getContextMenuItems, { ContextMenuActions } from "../ContextMenu/items";
-import { getCollection } from "../../state/selection/selectors";
 import { FileExplorerServiceBaseUrl } from "../../constants";
 
 /**
@@ -20,20 +19,17 @@ export default function useFileAccessContextMenu(filters?: FileFilter[], onDismi
     const fileSelection = useSelector(selection.selectors.getFileSelection);
     const userSelectedApplications = useSelector(interaction.selectors.getUserSelectedApplications);
     const { executionEnvService } = useSelector(interaction.selectors.getPlatformDependentServices);
-
     const [hasPlateBarCode, setHasPlateBarCode] = React.useState(false);
     let plateUILink: string | undefined = undefined;
 
     fileSelection.fetchAllDetails().then((fileDetails) => {
-        console.log(fileDetails);
-        const annotations = fileDetails[0].annotations;
-        const platebarcode = annotations.find((x) => x.name === "Plate Barcode");
+        // Grabbing plat barcode
+        const platebarcode = fileDetails[0].annotations.find((x) => x.name === "Plate Barcode");
+
+        //If theres a barcode make plateUI option available
         if (platebarcode != undefined && platebarcode.values != undefined) {
-            console.log("has barcode");
-            console.log(getCollection);
             setHasPlateBarCode(true);
             const barcode = platebarcode.values[0];
-            console.log(global.fileExplorerServiceBaseUrl);
             if (global.fileExplorerServiceBaseUrl === FileExplorerServiceBaseUrl.STAGING) {
                 plateUILink = `http://stg-aics.corp.alleninstitute.org/labkey/aics_microscopy/AICS/editPlate.view?Barcode=${barcode}`;
             } else if (
@@ -44,22 +40,6 @@ export default function useFileAccessContextMenu(filters?: FileFilter[], onDismi
         }
         return;
     });
-    // const onDetailsLoad = async () => {
-    //     console.log(fileDetails)
-    //     const annotations = fileDetails[0].annotations;
-    //     const platebarcode = annotations.find((x) => x.name === "Plate Barcode");
-    //     if (platebarcode != undefined && platebarcode.values != undefined) {
-    //         console.log("this has a barcode")
-    //         openPlateUIOption = {
-    //             key: ContextMenuActions.OPEN_PLATE_UI,
-    //             text: "Open Plate UI",
-    //             title: "Open this plate in the Plate UI",
-    //             href: "www.google.com",
-    //         };
-    //         openWithOptions.push(openPlateUIOption)
-    //     }
-    //     return await fileSelection.fetchAllDetails();
-    // }
 
     return React.useCallback(
         (evt: React.MouseEvent) => {
