980ae5bb0dcb6497195fd1b2c816491716bb27e9
SeanLeRoy <seanm@alleninstitute.org>
2024-07-01 15:34:41 -0700

    Add feature screenshots and slide functionality

--- packages/web/src/components/Home/FeatureOptions.ts
+++ packages/web/src/components/Home/FeatureOptions.ts
@@ -1,37 +1,121 @@
-export interface Feature {
-    id: number;
-    text: string; // Left menu
+interface Slide {
     caption: string; // Caption above image
-    imgSrc?: string; // Image src
+    imgSrc: string; // Image src
 }
 
-export const features = [
+interface Feature {
+    id: number;
+    text: string; // Left menu
+    slides: Slide[]; // Right carousel
+}
+
+const FEATURE_ASSET_FOLDER = "../../assets/features";
+
+export default [
     {
         id: 1,
         text: "Find target files quickly",
-        caption: `Filter, sort, group or search for your files
-      in a predictable and standard way and view thumbnails for 
-      quick identification. Switch to thumbnail view for...this 
-      is placeholder text for now.`,
+        slides: [
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/FindingData/Group.png`,
+                caption: `Unlike traditional file systems BioFile Finder allows you to dynamically generate your folder structure based on the metadata of your files. In this screenshot, the files are organized by (Grouped by) the "Gene" and "Structure" columns found in the selected Data source.`,
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/FindingData/FilterColumn.png`,
+                caption: `Filter by selecting a column to filter by...`,
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/FindingData/FilterValue.png`,
+                caption: `...then selecting a value for that column to filter by.`,
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/FindingData/Sort.png`,
+                caption: `Sort by clicking on the column header or the "Sort" button.`,
+            },
+        ],
     },
     {
         id: 2,
-        text: "Create new or combine subsets",
-        caption: "Placeholder text about creating or combining",
+        text: "Create new data sources or combine them",
+        slides: [
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/Datasets/LoadSource.png`,
+                caption:
+                    "Data can be loaded from a CSV, Parquet, or JSON file. The data source must include a column that contains the file path to the data. The data source can be loaded from a URL or uploaded from your local machine.",
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/Datasets/AddDataSource.png`,
+                caption:
+                    "Adding a second, third, fourth, etc. data source works by simply repeating the process.",
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/Datasets/GroupedDataSource.png`,
+                caption:
+                    'After you have added your data you can filter, group, and sort it however; this screenshot shows grouping by an automatically generated column "Data source" which represents the Data source each file came from.',
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/Datasets/SaveMetadataAs.png`,
+                caption:
+                    'New data sources can be generated from any number of existing data sources (filtered or whole). Right-click to open a context menu over your file selection then select "Save metadata as" then your preferred file format.',
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/Datasets/NewDataSourceColumns.png`,
+                caption:
+                    "The application will ask you which columns you would like included in the resulting new data source.",
+            },
+        ],
     },
     {
         id: 3,
-        text: "Directly open images in popular apps",
-        caption: "Placeholder text about directly opening images",
+        text: "Directly open images in a web-based viewer",
+        slides: [
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/Viewers/ContextMenu.png`,
+                caption:
+                    "Files can be interacted with via this application, right-clicking your file selection will present a context menu where you can select the application to open the files in. This example highlights the '3D Web Viewier' a free open source 3D web viewer also developed by AICS.",
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/Viewers/WebViewer.png`,
+                caption:
+                    "This is that same file we were looking at in the context menu, now opened in the 3D Web Viewer instantly from BioFile Finder.",
+            },
+        ],
     },
     {
         id: 4,
         text: "Share via URL, code snippet, or download",
-        caption: "Placeholder text about sharing code snippets",
+        slides: [
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/Share/URL.png`,
+                caption:
+                    "Your exact view (filters, groups, sorts, open folders) can be shared via URL. This URL can be shared with anyone and they will see the exact same view you are seeing assuming the data source is accessible to them. Local files for example will have to be re-selected by the user.",
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/Share/CodeSnippet.png`,
+                caption:
+                    "A code snippet can be generated that will recreate your exact view (filters, groups, sorts) programmatically. This could be useful for sharing your view with a colleague who is working in a Jupyter notebook for example.",
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/Share/Download.png`,
+                caption:
+                    "Lastly, BioFile Finder can be used as an interface for downloading files. Select the file you want to download, right-click to open the context menu, and select 'Download' to download the focused file.",
+            },
+        ],
     },
     {
         id: 5,
-        text: "View OME.Zarr files instantly using the web-based 3D Volume Viewer",
-        caption: "Placeholder text about viewing OME.Zarr files",
+        text: "View OME.Zarr or pre-generated thumbnails previews of files instantly",
+        slides: [
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/OmeZarr/Zarr.png`,
+                caption:
+                    "OME.Zarr files are a format for storing multi-dimensional arrays in a chunked, compressed, and efficient manner. BioFile Finder can read these files and preview them as thumbnails automatically.",
+            },
+            {
+                imgSrc: `${FEATURE_ASSET_FOLDER}/OmeZarr/Thumbnail.png`,
+                caption:
+                    'For other file formats, BioFile Finder can render pre-generated thumbnails for quick previewing. To do so, in your CSV, Parquet, or JSON file, include a column that contains the file path to the thumbnail image named "Thumbnail"',
+            },
+        ],
     },
-];
+] as Feature[];
--- packages/web/src/components/Home/Features.tsx
+++ packages/web/src/components/Home/Features.tsx
@@ -1,32 +1,86 @@
 import classNames from "classnames";
 import * as React from "react";
 
-import { Feature, features } from "./FeatureOptions";
+import FEATURE_OPTIONS from "./FeatureOptions";
 
 import styles from "./Features.module.css";
 
-// Placeholder for the splash page
+/**
+ * Component responsible for rendering the features section of the home page.
+ */
 export default function Features() {
-    const [caption, setCaption] = React.useState<string>(features[0].caption);
-    // FluentUI v8 doesn't have a tab component, so enforcing active state manually
-    const [activeId, setActiveId] = React.useState(1);
+    const [{ activeFeatureIndex, activeSlideIndex }, setActiveSlideIndices] = React.useState({
+        activeFeatureIndex: 0,
+        activeSlideIndex: 0,
+    });
+    const activeFeature = FEATURE_OPTIONS[activeFeatureIndex];
+    const activeSlide = activeFeature.slides[activeSlideIndex];
 
-    const setRightPanel = (feature: Feature) => {
-        setActiveId(feature.id);
-        setCaption(feature.caption);
-    };
+    const changeFeature = React.useCallback(
+        (featureIndex: number, slideIndex = 0) => {
+            if (activeFeatureIndex > FEATURE_OPTIONS.length - 1) {
+                throw new Error("Invalid feature index");
+            }
+
+            setActiveSlideIndices({
+                activeFeatureIndex: featureIndex,
+                activeSlideIndex: slideIndex,
+            });
+        },
+        [activeFeatureIndex, activeSlideIndex, setActiveSlideIndices]
+    );
+
+    React.useEffect(() => {
+        const handleNextOrPrevKeyPress = (event: KeyboardEvent) => {
+            const isHittingNext = event.key === "ArrowRight";
+            const isHittingPrev = event.key === "ArrowLeft";
+
+            if (isHittingNext) {
+                const hasNextSlide = activeSlideIndex < activeFeature.slides.length - 1;
+                const hasNextFeature = activeFeatureIndex < FEATURE_OPTIONS.length - 1;
+
+                if (hasNextSlide) {
+                    changeFeature(activeFeatureIndex, activeSlideIndex + 1);
+                } else if (hasNextFeature) {
+                    changeFeature(activeFeatureIndex + 1);
+                }
+            } else if (isHittingPrev) {
+                const hasPrevSlide = activeSlideIndex > 0;
+                const hasPrevFeature = activeFeatureIndex > 0;
+
+                if (hasPrevSlide) {
+                    changeFeature(activeFeatureIndex, activeSlideIndex - 1);
+                } else if (hasPrevFeature) {
+                    const newFeatureIndex = activeFeatureIndex - 1;
+                    changeFeature(
+                        newFeatureIndex,
+                        FEATURE_OPTIONS[newFeatureIndex].slides.length - 1
+                    );
+                }
+            }
+        };
+
+        // attach the event listener
+        document.addEventListener("keydown", handleNextOrPrevKeyPress);
+
+        // remove the event listener
+        return () => {
+            document.removeEventListener("keydown", handleNextOrPrevKeyPress);
+        };
+    }, [activeFeature, activeSlideIndex, setActiveSlideIndices, changeFeature]);
 
     return (
         <div className={styles.flexContainer}>
             <div className={styles.features}>
-                {features.map((feature) => (
+                {FEATURE_OPTIONS.map((feature, index) => (
                     <button
                         className={classNames({
-                            [styles.selectedFeature]: activeId === feature.id,
+                            // FluentUI v8 doesn't have a tab component, so enforcing active state manually
+                            [styles.selectedFeature]: activeFeature.id === feature.id,
                         })}
-                        onClick={() => setRightPanel(feature)}
+                        onClick={() => changeFeature(index)}
                         role="tab"
-                        aria-selected={activeId === feature.id}
+                        aria-selected={activeFeature.id === feature.id}
                         key={`feature-${feature.id}`}
                     >
                         {feature.text}
@@ -34,8 +88,21 @@ export default function Features() {
                 ))}
             </div>
             <div className={styles.carousel}>
-                {caption}
-                <div className={styles.carouselImage} />
+                <img height={300} src="fms-file-explorer-web/assets/banner.png" />
+                <div className={styles.slideButtonsContainer}>
+                    {activeFeature.slides.map((_, index) => (
+                        <button
+                            className={classNames(styles.slideButton, {
+                                [styles.selectedSlide]: activeSlideIndex === index,
+                            })}
+                            onClick={() => changeFeature(activeFeatureIndex, index)}
+                            role="tab"
+                            aria-selected={activeSlideIndex === index}
+                            key={`slide-${index}`}
+                        />
+                    ))}
+                </div>
+                <p>{activeSlide.caption}</p>
             </div>
         </div>
     );
