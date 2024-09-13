3ef3afdc10bea0e463a20a804ad3ac3eb923f86a
Anya Wallace <anya.wallace@alleninstitute.org>
2024-09-13 15:36:06 -0700

    Temporarily undo empty list changes

---
+++ packages/core/components/EmptyFileListMessage/FilterList.tsx
@@ -0,0 +1,62 @@
+import classNames from "classnames";
+import { map } from "lodash";
+import * as React from "react";
+import styles from "./FilterList.module.css";
+import { Filter } from "../../entity/FileFilter";
+
+interface Props {
+    name: string;
+    filters: Filter[];
+}
+
+/**
+ * UI for displaying the annotation values applied as file filters. Each `FileFilter` within `props.filters`
+ * must relate to the same annotation (e.g. Each `FileFilter::name` should be equal).
+ * Logic is based on the FilterMedallion component
+ */
+export default function FilterList(props: Props) {
+    const { filters, name } = props;
+
+    const [expanded, setExpanded] = React.useState(false);
+
+    // Determine if filter has reached its max-width and text is overflowing
+    // If a filter is no longer overflowing but its "expanded" state is truthy, reset.
+    const [overflowing, setOverflowing] = React.useState(false);
+    const textRef = React.useRef<HTMLElement | null>(null);
+    React.useEffect(() => {
+        if (textRef.current) {
+            const width = textRef.current.clientWidth;
+            const scrollWidth = textRef.current.scrollWidth;
+            const isOverflowing = scrollWidth > width;
+            setOverflowing(isOverflowing);
+            if (!isOverflowing) {
+                setExpanded(false);
+            }
+        } else {
+            setOverflowing(false);
+        }
+    }, [textRef, filters]);
+
+    const firstFilterValue = filters[0].value.toString();
+    let operator = "equal to";
+    if (filters.length > 1) operator = "for values of";
+    else if (firstFilterValue.includes("RANGE")) operator = "between";
+    const valueDisplay = map(filters, (filter) => filter.displayValue).join(", ");
+    const display = ` ${operator} ${valueDisplay}`;
+
+    return (
+        <span className={styles.filter}>
+            <span
+                className={classNames(styles.filterText, {
+                    [styles.expandable]: overflowing,
+                    [styles.expanded]: expanded,
+                })}
+                onClick={() => overflowing && setExpanded((prev) => !prev)}
+                ref={textRef}
+                title={name + display}
+            >
+                <b>{name}</b> {display}
+            </span>
+        </span>
+    );
+}
