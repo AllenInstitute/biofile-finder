cc7dc67a2a11c48a253728bf02f481e78cce36b7
SeanLeRoy <seanm@alleninstitute.org>
2024-10-07 14:44:07 -0700

    Update to use FES stored annotationID

--- packages/core/state/interaction/logics.ts
+++ packages/core/state/interaction/logics.ts
@@ -510,6 +510,9 @@ const editFilesLogic = createLogic({
         const fileService = interactionSelectors.getFileService(deps.getState());
         const fileSelection = selection.selectors.getFileSelection(deps.getState());
         const sortColumn = selection.selectors.getSortColumn(deps.getState());
+        const annotationNameToAnnotationMap = metadata.selectors.getAnnotationNameToAnnotationMap(
+            deps.getState()
+        );
         const {
             payload: { annotations, filters },
         } = deps.action as EditFiles;
@@ -565,7 +568,11 @@ const editFilesLogic = createLogic({
                     (fileId) =>
                         new Promise<void>(async (resolve, reject) => {
                             try {
-                                await fileService.editFile(fileId, annotations);
+                                await fileService.editFile(
+                                    fileId,
+                                    annotations,
+                                    annotationNameToAnnotationMap
+                                );
                                 totalFileEdited += 1;
                                 onProgress();
                                 resolve();
