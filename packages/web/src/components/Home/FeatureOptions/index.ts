6181a16902cb57d412f552b5504170c99c7e74c4
SeanLeRoy <seanm@alleninstitute.org>
2024-10-24 14:43:30 -0700

    Replace homepage images

---
+++ packages/web/src/components/Home/FeatureOptions/index.ts
@@ -0,0 +1,7 @@
+import OmeZarr from "./OmeZarr";
+import Share from "./Share";
+import Viewers from "./Viewers";
+import Datasets from "./Datasets";
+import FindingData from "./FindingData";
+
+export default [FindingData, Datasets, Viewers, Share, OmeZarr];
