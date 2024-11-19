fc8d6877299bcec2796c816d9b867547641e1002
Anya Wallace <anya.wallace@alleninstitute.org>
2024-11-19 14:51:08 -0800

    create edit metadata modal

---
+++ packages/core/components/EditMetadata/NewAnnotationPathway.tsx
@@ -0,0 +1,168 @@
+import { IComboBoxOption, IconButton, Stack, StackItem, TextField } from "@fluentui/react";
+import classNames from "classnames";
+import * as React from "react";
+
+import MetadataDetails, { ValueCountItem } from "./MetadataDetails";
+import { PrimaryButton, SecondaryButton } from "../Buttons";
+import ComboBox from "../ComboBox";
+import Tooltip from "../Tooltip";
+import { AnnotationType } from "../../entity/AnnotationFormatter";
+
+import styles from "./EditMetadata.module.css";
+
+enum EditStep {
+    PASSWORD = 0, // Placeholder
+    CREATE_FIELD = 1,
+    EDIT_FILES = 2,
+}
+
+interface NewAnnotationProps {
+    onDismiss: () => void;
+    selectedFileCount: number;
+}
+
+/**
+ * Component for submitting a new annotation
+ * and then entering values for the selected files
+ */
+export default function NewAnnotationPathway(props: NewAnnotationProps) {
+    const [step, setStep] = React.useState<EditStep>(EditStep.CREATE_FIELD);
+    const [_newValues, setNewValues] = React.useState<string | undefined>();
+    const [newFieldName, setNewFieldName] = React.useState<string>("");
+    const [newFieldDataType, setNewFieldDataType] = React.useState<string | undefined>();
+    const [newDropdownOption, setNewDropdownOption] = React.useState<string>("");
+    const [dropdownOptions, setDropdownOptions] = React.useState<IComboBoxOption[]>([]);
+
+    const addDropdownChip = (evt: React.FormEvent) => {
+        evt.preventDefault();
+        if (
+            newDropdownOption &&
+            !dropdownOptions.filter((opt) => opt.key === newDropdownOption).length
+        ) {
+            const newOptionAsIComboBox: IComboBoxOption = {
+                key: newDropdownOption,
+                text: newDropdownOption,
+            };
+            setDropdownOptions([...dropdownOptions, newOptionAsIComboBox]);
+            setNewDropdownOption("");
+        }
+    };
+
+    const removeDropdownChip = (optionToRemove: IComboBoxOption) => {
+        setDropdownOptions(dropdownOptions.filter((opt) => opt !== optionToRemove));
+    };
+
+    function onSubmit() {
+        // TO DO: endpoint logic is in progress on a different branch
+        props.onDismiss();
+    }
+
+    return (
+        <>
+            {/* TO DO: Prevent user from entering a name that collides with existing annotation */}
+            <TextField
+                required
+                label="New metadata field name"
+                className={styles.textField}
+                onChange={(_, newValue) => setNewFieldName(newValue || "")}
+                placeholder="Add a new field name..."
+                value={newFieldName}
+            />
+            {step === EditStep.CREATE_FIELD && (
+                <>
+                    <ComboBox
+                        className={styles.comboBox}
+                        defaultValue={newFieldDataType || undefined}
+                        label="Data type"
+                        placeholder="Select a data type"
+                        options={Object.values(AnnotationType).map((type, index) => {
+                            return {
+                                key: `datatype-${index}`,
+                                text: type,
+                            };
+                        })}
+                        useComboBoxAsMenuWidth
+                        onChange={(option) => setNewFieldDataType(option?.text || "")}
+                    />
+                    {newFieldDataType === AnnotationType.DROPDOWN && (
+                        <>
+                            <form onSubmit={addDropdownChip}>
+                                <TextField
+                                    label="Add dropdown options"
+                                    className={styles.textField}
+                                    onChange={(_, newValue) => setNewDropdownOption(newValue || "")}
+                                    placeholder="Type an option name..."
+                                    iconProps={{
+                                        className: classNames(styles.submitIcon),
+                                        iconName: "ReturnKey",
+                                        onClick: newDropdownOption ? addDropdownChip : undefined,
+                                    }}
+                                    value={newDropdownOption}
+                                />
+                            </form>
+                            <div>
+                                {dropdownOptions.map((option) => (
+                                    <div
+                                        key={option.key}
+                                        className={styles.selectedOptionContainer}
+                                    >
+                                        <Tooltip content={option.text}>
+                                            <p className={styles.selectedOption}>{option.text}</p>
+                                        </Tooltip>
+                                        <IconButton
+                                            className={styles.selectedOptionButton}
+                                            iconProps={{ iconName: "Cancel" }}
+                                            onClick={() => removeDropdownChip(option)}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                        </>
+                    )}
+                </>
+            )}
+            {step === EditStep.EDIT_FILES && (
+                <MetadataDetails
+                    onChange={(value) => setNewValues(value)}
+                    items={[
+                        {
+                            value: undefined,
+                            fileCount: props.selectedFileCount,
+                        } as ValueCountItem,
+                    ]}
+                />
+            )}
+            <div className={styles.footer}>
+                <Stack horizontal>
+                    <StackItem>
+                        {step === EditStep.EDIT_FILES && (
+                            <SecondaryButton
+                                title="Go back"
+                                text="BACK"
+                                onClick={() => setStep(EditStep.CREATE_FIELD)}
+                            />
+                        )}
+                    </StackItem>
+                    <StackItem grow align="end" className={styles.footerAlignRight}>
+                        <SecondaryButton title="Cancel" text="CANCEL" onClick={props.onDismiss} />
+                        {step === EditStep.CREATE_FIELD && (
+                            <PrimaryButton
+                                disabled={
+                                    !newFieldName ||
+                                    (newFieldDataType === AnnotationType.DROPDOWN &&
+                                        !dropdownOptions.length)
+                                }
+                                title="Next"
+                                text="NEXT"
+                                onClick={() => setStep(EditStep.EDIT_FILES)}
+                            />
+                        )}
+                        {step === EditStep.EDIT_FILES && (
+                            <PrimaryButton title="Submit" text="SUBMIT" onClick={onSubmit} />
+                        )}
+                    </StackItem>
+                </Stack>
+            </div>
+        </>
+    );
+}
---
+++ packages/core/components/EditMetadata/index.tsx
@@ -0,0 +1,103 @@
+import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import ExistingAnnotationPathway from "./ExistingAnnotationPathway";
+import NewAnnotationPathway from "./NewAnnotationPathway";
+import ChoiceGroup from "../ChoiceGroup";
+import { TOP_LEVEL_FILE_ANNOTATION_NAMES } from "../../constants";
+import { interaction, metadata, selection } from "../../state";
+
+import styles from "./EditMetadata.module.css";
+
+enum EditMetadataPathway {
+    EXISTING = "existing",
+    NEW = "new",
+}
+
+/**
+ * Form that acts as a wrapper for both metadata editing pathways (new vs existing annotations).
+ * Performs all necessary state selections on render and passes data as props to child components
+ */
+export default function EditMetadataForm() {
+    const dispatch = useDispatch();
+    const fileSelection = useSelector(selection.selectors.getFileSelection);
+    const onDismiss = () => {
+        dispatch(interaction.actions.hideVisibleModal());
+    };
+    // Don't allow users to edit top level annotations (e.g., File Name)
+    const annotationOptions = useSelector(metadata.selectors.getSortedAnnotations)
+        .filter((annotation) => !TOP_LEVEL_FILE_ANNOTATION_NAMES.includes(annotation.name))
+        .map((annotation) => {
+            return {
+                key: annotation.name,
+                text: annotation.displayName,
+            };
+        });
+    const [editPathway, setEditPathway] = React.useState<EditMetadataPathway>(
+        EditMetadataPathway.EXISTING
+    );
+    const [annotationValueMap, setAnnotationValueMap] = React.useState<Map<string, any>>();
+
+    React.useEffect(() => {
+        fileSelection.fetchAllDetails().then((fileDetails) => {
+            const annotationMapping = new Map();
+            // Group details by annotation with a count for each value
+            fileDetails.forEach((file) => {
+                file.annotations.map((annotation) => {
+                    // For now, if a file has multiple values for an annotation it should be considered a distinct set
+                    const joinedValues = annotation.values.join(", ");
+                    if (!annotationMapping.has(annotation.name)) {
+                        annotationMapping.set(annotation.name, { [joinedValues]: 1 });
+                    } else {
+                        const existing = annotationMapping.get(annotation.name);
+                        annotationMapping.set(annotation.name, {
+                            ...existing,
+                            [joinedValues]: existing?.[joinedValues]
+                                ? existing[joinedValues] + 1
+                                : 1,
+                        });
+                    }
+                });
+            });
+            setAnnotationValueMap(annotationMapping);
+        });
+    }, [fileSelection]);
+
+    return (
+        <>
+            <ChoiceGroup
+                defaultSelectedKey={editPathway}
+                onChange={(_ev, option) =>
+                    setEditPathway(
+                        (option?.key as EditMetadataPathway) || EditMetadataPathway.EXISTING
+                    )
+                }
+                options={[
+                    {
+                        key: EditMetadataPathway.EXISTING,
+                        text: "Existing field",
+                    },
+                    {
+                        key: EditMetadataPathway.NEW,
+                        text: "New field",
+                    },
+                ]}
+            />
+            <div className={styles.contentWrapper}>
+                {editPathway === EditMetadataPathway.EXISTING ? (
+                    <ExistingAnnotationPathway
+                        onDismiss={onDismiss}
+                        annotationValueMap={annotationValueMap}
+                        annotationOptions={annotationOptions}
+                        selectedFileCount={fileSelection.count()}
+                    />
+                ) : (
+                    <NewAnnotationPathway
+                        onDismiss={onDismiss}
+                        selectedFileCount={fileSelection.count()}
+                    />
+                )}
+            </div>
+        </>
+    );
+}
