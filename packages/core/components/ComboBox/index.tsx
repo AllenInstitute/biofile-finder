678723364e69ba6e05c1ce5d3d8f70a6cafa12d4
Anya Wallace <anya.wallace@alleninstitute.org>
2024-12-06 10:35:25 -0800

    minor component fixes

--- packages/core/components/ComboBox/index.tsx
+++ packages/core/components/ComboBox/index.tsx
@@ -66,7 +66,7 @@ export default function BaseComboBox(props: Props) {
             comboBoxOptionStyles={{
                 rootChecked: styles.comboBoxItemChecked,
             }}
-            useComboBoxAsMenuWidth
+            useComboBoxAsMenuWidth={true}
         />
     );
 }
