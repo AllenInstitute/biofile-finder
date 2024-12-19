083456f42c8a8a3a27e0e6503df236d33c0bcd7a
Philip Garrison <philip.garrison@alleninstitute.org>
2024-12-19 15:56:02 -0800

    368 Test for new HTTPAnnotationService special case

--- packages/core/services/AnnotationService/HttpAnnotationService/test/HttpAnnotationService.test.ts
+++ packages/core/services/AnnotationService/HttpAnnotationService/test/HttpAnnotationService.test.ts
@@ -31,6 +31,16 @@ describe("HttpAnnotationService", () => {
             );
             expect(annotations[0]).to.be.instanceOf(Annotation);
         });
+
+        it("renames Local File Path to File Path (Local)", async () => {
+            const annotationService = new HttpAnnotationService({
+                fileExplorerServiceBaseUrl: FESBaseUrl.TEST,
+                httpClient,
+            });
+            const annotations = await annotationService.fetchAnnotations();
+            const localPathAnnotation = annotations.find((a) => a.name === "Local File Path");
+            expect(localPathAnnotation?.displayName).to.equal("File Path (Local)");
+        });
     });
 
     describe("fetchAnnotationValues", () => {
