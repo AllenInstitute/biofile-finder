4ebcb08a05607fb1984e1bb7b77a7a6ac1fc03c6
Brian Whitney <brian.whitney@alleninstitute.org>
2025-01-17 12:05:42 -0800

    try to update test env

--- packages/desktop/scripts/test-env-setup.js
+++ packages/desktop/scripts/test-env-setup.js
@@ -3,3 +3,17 @@
  * See the `test:renderer` npm script.
  */
 require("regenerator-runtime/runtime");
+
+const sinon = require("sinon");
+const electron = require("electron");
+
+// Create a global mock for ipcRenderer
+const ipcRendererMock = {
+    invoke: sinon.stub(),
+    send: sinon.stub(),
+    on: sinon.stub(),
+    removeListener: sinon.stub(),
+};
+
+// Replace Electron's ipcRenderer with the mock
+electron.ipcRenderer = ipcRendererMock;
