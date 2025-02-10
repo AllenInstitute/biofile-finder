7cab70b64d185788790bcfd969077828beadbd02
Anya Wallace <anya.wallace@alleninstitute.org>
2025-02-10 15:46:26 -0800

    fix broken unit tests

--- packages/core/entity/Annotation/mocks.ts
+++ packages/core/entity/Annotation/mocks.ts
@@ -1,38 +1,40 @@
+import { AnnotationType } from "../AnnotationFormatter";
+
 export const annotationsJson = [
     {
         annotationDisplayName: "Date created",
         annotationName: "date_created",
         description: "Date and time file was created",
-        type: "Date/Time",
+        type: AnnotationType.DATETIME,
     },
     {
         annotationDisplayName: "Cell line",
         annotationName: "cell_line",
         description: "AICS cell line",
-        type: "Text",
+        type: AnnotationType.STRING,
     },
     {
         annotationDisplayName: "Cells are dead",
         annotationName: "cell_dead",
         description: "Does this field contain dead cells",
-        type: "Yes/No",
+        type: AnnotationType.BOOLEAN,
     },
     {
         annotationDisplayName: "Is matrigel hard?",
         annotationName: "matrigel_hardened",
         description: "Whether or not matrigel is hard.",
-        type: "Yes/No",
+        type: AnnotationType.BOOLEAN,
     },
     {
         annotationDisplayName: "Objective",
         annotationName: "objective",
         description: "Imaging objective",
-        type: "Number",
+        type: AnnotationType.NUMBER,
     },
     {
         annotationName: "Local File Path",
         annotationDisplayName: "Local File Path",
         description: "Path to file in on-premises storage.",
-        type: "Text",
+        type: AnnotationType.STRING,
     },
 ];
