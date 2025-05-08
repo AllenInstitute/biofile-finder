44b090450d73f6c0f8ddb5accaed0a8abfe264fe
Anya Wallace <anya.wallace@alleninstitute.org>
2025-05-08 16:44:25 -0700

    attempt to fix file list line height issue

--- packages/core/components/FileList/index.tsx
+++ packages/core/components/FileList/index.tsx
@@ -40,8 +40,8 @@ const DEFAULTS = {
 };
 
 const MAX_NON_ROOT_HEIGHT = 300;
-const SMALL_ROW_HEIGHT = 17;
-const TALL_ROW_HEIGHT = 19;
+const SMALL_ROW_HEIGHT = 20;
+const TALL_ROW_HEIGHT = 24;
 
 /**
  * Wrapper for react-window-infinite-loader and react-window that knows how to lazily fetch its own data. It will lay
