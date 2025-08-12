9ff8eb6a02924f1fe63490c445fe804ea85b2705
Peyton Lee <peyton.lee@alleninstitute.org>
2025-08-12 14:32:59 -0700

    feat: Added exponential backoff for remote file server check

--- packages/core/hooks/useRemoteFileUpload.ts
+++ packages/core/hooks/useRemoteFileUpload.ts
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect } from "react";
-
 const REMOTE_SERVER_URL = "http://dev-aics-dtp-001.corp.alleninstitute.org:8080";
 const API_PING = "/ping";
 const API_UPLOAD = "/upload";
@@ -8,20 +7,26 @@ const API_GET_FILE = "/get-file";
 type UploadResponse = { id: string; url: string };
 type UploadFileCallback = (file: File) => Promise<UploadResponse>;
 
+const MAX_FETCH_ATTEMPTS = 5;
+
 const useRemoteFileUpload = (): [hasRemoteServer: boolean, uploadCsv: UploadFileCallback] => {
     const [hasRemoteServer, setHasRemoteServer] = React.useState(false);
 
     useEffect(() => {
         // TODO: Have the check perform on a regular interval
-        // and use exponential backoff when the server is not reachable.
         const checkRemoteServer = async () => {
             try {
-                const response = await fetch(`${REMOTE_SERVER_URL}${API_PING}`);
-                if (response.ok) {
-                    setHasRemoteServer(true);
-                } else {
-                    console.error("Remote server is not reachable");
+                for (let attempt = 1; attempt <= MAX_FETCH_ATTEMPTS; attempt++) {
+                    const response = await fetch(`${REMOTE_SERVER_URL}${API_PING}`);
+                    if (response.ok) {
+                        setHasRemoteServer(true);
+                        return;
+                    }
+                    await new Promise((resolve) =>
+                        setTimeout(resolve, Math.pow(2, attempt) * 1000)
+                    );
                 }
+                console.error("Remote file upload server is not reachable.");
             } catch (error) {
                 console.error("Error checking remote server:", error);
             }
