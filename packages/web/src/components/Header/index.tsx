9b458f495280d705f63de560355b58a87e2e9959
SeanLeRoy <seanme97@gmail.com>
2025-08-22 14:08:52 -0700

    Fix menu for phones

--- packages/web/src/components/Header/index.tsx
+++ packages/web/src/components/Header/index.tsx
@@ -1,24 +1,15 @@
-import { DirectionalHint, PrimaryButton, Stack } from "@fluentui/react";
 import * as React from "react";
-import { useDispatch } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 
+import Menu from "./Menu";
 import { APPLICATION_NAME } from "../../constants";
 import SiteLogo from "../../../assets/site-logo.png";
-import { SecondaryButton, useButtonMenu } from "../../../../core/components/Buttons";
-import useHelpOptions from "../../../../core/hooks/useHelpOptions";
 
 import styles from "./Header.module.css";
 
 export default function Header() {
-    const dispatch = useDispatch();
     const currentPath = useLocation().pathname;
     const isApp: boolean = currentPath == "/app";
-    const helpMenuOptions = useHelpOptions(dispatch, true, isApp);
-    const helpMenu = useButtonMenu({
-        items: helpMenuOptions,
-        directionalHint: DirectionalHint.bottomAutoEdge,
-    });
 
     return (
         <>
@@ -35,53 +26,7 @@ export default function Header() {
                         <h6>Powered by BigFile Finder</h6>
                     </div>
                 </Link>
-                <Stack
-                    reversed
-                    horizontal
-                    wrap
-                    styles={{ root: styles.stack }}
-                    tokens={{ childrenGap: 5 }}
-                >
-                    <Stack.Item styles={{ root: styles.stackItem }}>
-                        {currentPath !== "/app" && (
-                            <Link to="app">
-                                <SecondaryButton
-                                    className={styles.startButton}
-                                    title="Get started in the app"
-                                    text="GET STARTED"
-                                />
-                            </Link>
-                        )}
-                    </Stack.Item>
-                    <Stack.Item grow styles={{ root: styles.stackItem }}>
-                        <div className={styles.right}>
-                            <Link
-                                to="datasets"
-                                className={styles.routeLink}
-                                target={isApp ? "_blank" : "_self"}
-                                rel="noreferrer"
-                            >
-                                Open-source datasets
-                            </Link>
-                            <Link
-                                to="learn"
-                                className={styles.routeLink}
-                                target={isApp ? "_blank" : "_self"}
-                                rel="noreferrer"
-                            >
-                                Learn
-                            </Link>
-                            <PrimaryButton
-                                ariaLabel="Help"
-                                className={styles.helpMenuButton}
-                                styles={{ label: styles.helpMenuLabel }}
-                                menuIconProps={{ iconName: "ChevronDown" }}
-                                menuProps={helpMenu}
-                                text="Help"
-                            />
-                        </div>
-                    </Stack.Item>
-                </Stack>
+                <Menu />
             </div>
         </>
     );
