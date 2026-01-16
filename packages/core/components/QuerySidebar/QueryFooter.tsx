4b6dae6a712784df6f006f9e63e8818ffb316b24
Anya Wallace <anya.wallace@alleninstitute.org>
2026-01-16 12:23:37 -0800

    Remove call to fetch total count from query footer

--- packages/core/components/QuerySidebar/QueryFooter.tsx
+++ packages/core/components/QuerySidebar/QueryFooter.tsx
@@ -5,18 +5,14 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { TertiaryButton } from "../Buttons";
 import { ModalType } from "../Modal";
-import Tooltip from "../Tooltip";
 import Tutorial from "../../entity/Tutorial";
 import IncludeFilter from "../../entity/FileFilter/IncludeFilter";
-import FileSet from "../../entity/FileSet";
 import { SearchParamsComponents } from "../../entity/SearchParams";
 import useSaveMetadataOptions from "../../hooks/useSaveMetadataOptions";
 import { interaction, selection } from "../../state";
 import { Query } from "../../state/selection/actions";
 
 import styles from "./QueryFooter.module.css";
-
-const MAX_MANIFEST_FILE_COUNT = 250000;
 
 interface Props {
     isDeletable?: boolean;
@@ -32,37 +28,12 @@ export default function QueryFooter(props: Props) {
     const dispatch = useDispatch();
 
     const url = useSelector(selection.selectors.getEncodedSearchParams);
-    const fileService = useSelector(interaction.selectors.getFileService);
-    const [totalFileCount, setTotalFileCount] = React.useState(0);
     const combinedFilters = React.useMemo(() => {
         const groupByFilters = props.queryComponents.hierarchy.map(
             (annotationName) => new IncludeFilter(annotationName)
         );
         return [...props.queryComponents.filters, ...groupByFilters];
     }, [props.queryComponents.filters, props.queryComponents.hierarchy]);
-    const totalFileSet = React.useMemo(() => {
-        return new FileSet({
-            fileService,
-            filters: combinedFilters,
-        });
-    }, [fileService, combinedFilters]);
-
-    // Get a count of all files
-    React.useEffect(() => {
-        totalFileSet
-            .fetchTotalCount()
-            .then((count) => {
-                setTotalFileCount(count);
-            })
-            .catch((err) => {
-                // Data source may not be prepared if the data source is taking longer to load
-                // than the component does to render. In this case, we can ignore the error.
-                // The component will re-render when the data source is prepared.
-                if (!err?.message.includes("Data source is not prepared")) {
-                    throw err;
-                }
-            });
-    }, [totalFileSet, setTotalFileCount]);
 
     const isEmptyQuery = !props.query.parts.sources.length;
 
@@ -153,21 +124,13 @@ export default function QueryFooter(props: Props) {
                 onClick={onDuplicateQuery}
                 title="Duplicate query"
             />
-            <Tooltip
-                content={
-                    totalFileCount > MAX_MANIFEST_FILE_COUNT
-                        ? "Unable to save full result for >250,000 files"
-                        : undefined
-                }
-            >
-                <TertiaryButton
-                    invertColor
-                    disabled={isEmptyQuery || totalFileCount > MAX_MANIFEST_FILE_COUNT}
-                    iconName="Save"
-                    menuItems={saveQueryAsOptions}
-                    title="Save query result as..."
-                />
-            </Tooltip>
+            <TertiaryButton
+                invertColor
+                disabled={isEmptyQuery}
+                iconName="Save"
+                menuItems={saveQueryAsOptions}
+                title="Save query result as..."
+            />
             <TertiaryButton
                 invertColor
                 disabled={isEmptyQuery}
