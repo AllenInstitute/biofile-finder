a776a9820587420cb43b2226ea7630a833a85374
Anya Wallace <anya.wallace@alleninstitute.org>
2026-04-13 20:07:38 -0700

    Add version management to PersistentConfigService

--- packages/web/src/services/PersistentConfigServiceWeb.ts
+++ packages/web/src/services/PersistentConfigServiceWeb.ts
@@ -1,3 +1,5 @@
+import { lt, valid } from "semver";
+
 import {
     PersistentConfigService,
     PersistedConfig,
@@ -8,16 +10,44 @@ interface PersistentConfigServiceWebOptions {
     clearExistingData?: boolean;
 }
 
+// Manually manage version
+const CURRENT_VERSION = "1.0.0";
+const VERSION_KEY = "BFF_PERSISTED_CONFIG_VERSION";
+
 // Use browser localstorage to persist data between sessions
 export default class PersistentConfigServiceWeb implements PersistentConfigService {
     public constructor(options: PersistentConfigServiceWebOptions = {}) {
         if (options.clearExistingData) {
             localStorage.clear();
         }
+        // Check if localStorage has a stale version number
+        const storedVersion = localStorage.getItem(VERSION_KEY);
+        if (storedVersion !== CURRENT_VERSION) {
+            this.migrate(storedVersion);
+            // Update to new version
+            localStorage.setItem(VERSION_KEY, CURRENT_VERSION);
+        }
     }
 
-    public get(key: PersistedConfigKeys): any {
-        return localStorage.getItem(key);
+    // Handle version migrations.
+    // If we deprecate keys in the future, we can use this method to
+    // remove/update values as needed, e.g.,
+    //   if (gt(oldVersion, "1.0.1")) { // old version > 1.0.1
+    //.    if (localStorage.getItem(PersistedConfigKeys.DeprecatedKey)) {
+    //       localStorage.removeItem(PersistedConfigKeys.DeprecatedKey);
+    //     }
+    //   }
+    private migrate(oldVersion: string | null) {
+        // If invalid version, cannot validate stored keys
+        if (!oldVersion || lt(oldVersion, "0.0.0") || !valid(oldVersion)) {
+            this.clear();
+        }
+        return;
+    }
+
+    // localStorage only stores strings
+    public get(key: PersistedConfigKeys): string | undefined {
+        return localStorage.getItem(key) ?? undefined; // prefer undefined over null to match parent class
     }
 
     public getAll(): PersistedConfig {
@@ -29,12 +59,13 @@ export default class PersistentConfigServiceWeb implements PersistentConfigServi
             {}
         );
     }
+
     public clear(): void {
         localStorage.clear();
     }
 
     public persist(config: PersistedConfig): void;
-    public persist(key: PersistedConfigKeys, value: any): void;
+    public persist(key: PersistedConfigKeys, value?: string): void;
     public persist(arg: PersistedConfigKeys | PersistedConfig, value?: any) {
         if (typeof arg === "object") {
             Object.entries(arg as PersistedConfig).forEach(([key, value]) => {
@@ -43,6 +74,7 @@ export default class PersistentConfigServiceWeb implements PersistentConfigServi
         } else if (value === undefined || value === null) {
             localStorage.removeItem(arg);
         } else {
+            // setItem only accepts strings
             localStorage.setItem(arg, value);
         }
     }
