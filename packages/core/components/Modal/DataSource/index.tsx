b79ae13fb2d1620b8f15c74e768ecc3f2dd0bd29
Anya Wallace <anya.wallace@alleninstitute.org>
2026-07-07 09:56:08 -0700

    Update main data source modal to use template for optional files

--- packages/core/components/Modal/DataSource/index.tsx
+++ packages/core/components/Modal/DataSource/index.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
+import { useSelector } from "react-redux";
 
 import { ModalProps } from "..";
 import BaseModal from "../BaseModal";
-import DataSourcePrompt from "../../DataSourcePrompt";
+import DataSourcePrompt, { DataSourceType, DataSourceTypeToString } from "../../DataSourcePrompt";
+import { interaction } from "../../../state";
+import { DataSourcePromptInfo } from "../../../state/interaction/actions";
 
 import styles from "./DataSource.module.css";
 
@@ -10,10 +13,12 @@ import styles from "./DataSource.module.css";
  * Dialog meant to prompt user to select a data source option
  */
 export default function DataSource(props: ModalProps) {
+    const dataSourceInfo = useSelector(interaction.selectors.getDataSourceInfoForVisibleModal);
+    const { sourceType = DataSourceType.default } = dataSourceInfo || ({} as DataSourcePromptInfo);
     return (
         <BaseModal
             body={<DataSourcePrompt className={styles.content} isModal />}
-            title="Choose a data source"
+            title={`Choose a ${DataSourceTypeToString(sourceType)}`}
             onDismiss={props.onDismiss}
         />
     );
