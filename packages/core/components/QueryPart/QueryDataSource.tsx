0b331104a07511b7e127653981e1d0594278a8bc
Anya Wallace <anya.wallace@alleninstitute.org>
2026-07-23 12:06:16 -0700

    Parse sources from markdown source in Query components

--- packages/core/components/QueryPart/QueryDataSource.tsx
+++ packages/core/components/QueryPart/QueryDataSource.tsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 import QueryPart from ".";
 import { AICS_FMS_DATA_SOURCE_NAME } from "../../constants";
-import { Source } from "../../entity/SearchParams";
+import { getNameAndTypeFromSourceUrl, Source } from "../../entity/SearchParams";
 import { interaction, metadata, selection } from "../../state";
 
 interface Props {
@@ -21,6 +21,39 @@ export default function QueryDataSource(props: Props) {
     const selectedQuery = useSelector(selection.selectors.getSelectedQuery);
     const dataSources = useSelector(metadata.selectors.getDataSources);
     const selectedDataSources = useSelector(selection.selectors.getSelectedDataSources);
+    const markdownUrls = useSelector(selection.selectors.getDatasetUrlsFromMarkdown);
+    const datasetDescriptionSource = useSelector(selection.selectors.getDatasetDescriptionSource);
+    const { mainSources, columnDescriptionSource, provenanceSource } = React.useMemo(() => {
+        if (markdownUrls) {
+            const main_url = markdownUrls?.dataset_url;
+            const descriptions_url = markdownUrls?.descriptions_url;
+            const provenance_url = markdownUrls?.provenance_url;
+            return {
+                mainSources: main_url
+                    ? [{ ...getNameAndTypeFromSourceUrl(main_url), uri: main_url }]
+                    : props.dataSources,
+                columnDescriptionSource: descriptions_url
+                    ? { ...getNameAndTypeFromSourceUrl(descriptions_url), uri: descriptions_url }
+                    : props.sourceMetadata,
+                provenanceSource: provenance_url
+                    ? { ...getNameAndTypeFromSourceUrl(provenance_url), uri: provenance_url }
+                    : props.sourceProvenance,
+            };
+        } else {
+            return {
+                mainSources: props.dataSources,
+                columnDescriptionSource: props.sourceMetadata,
+                provenanceSource: props.sourceProvenance,
+            };
+        }
+    }, [markdownUrls, props.dataSources, props.sourceMetadata, props.sourceProvenance]);
+
+    const onShowDatasetInfo = () => {
+        if (!datasetDescriptionSource) return undefined;
+        else {
+            // TO DO: dispatch action when we have the UI component
+        }
+    };
 
     return (
         <QueryPart
@@ -36,6 +69,7 @@ export default function QueryDataSource(props: Props) {
                           )
                     : undefined
             }
+            onShowDatasetInfo={datasetDescriptionSource ? onShowDatasetInfo : undefined}
             addMenuListItems={[
                 {
                     key: "ADD DATA SOURCE",
@@ -85,23 +119,24 @@ export default function QueryDataSource(props: Props) {
                 },
             ]}
             rows={[
-                ...props.dataSources.map((dataSource) => ({
+                ...mainSources.map((dataSource) => ({
                     id: dataSource.name,
                     title: dataSource.name,
+                    datasetDescriptionSource: datasetDescriptionSource?.name,
                 })),
-                ...(props.sourceMetadata
+                ...(columnDescriptionSource
                     ? [
                           {
                               id: "sourceMetadata",
-                              title: `described by: ${props.sourceMetadata.name}`,
+                              title: `described by: ${columnDescriptionSource.name}`,
                           },
                       ]
                     : []),
-                ...(props.sourceProvenance
+                ...(provenanceSource
                     ? [
                           {
                               id: "sourceProvenance",
-                              title: `provenance from: ${props.sourceProvenance.name}`,
+                              title: `provenance from: ${provenanceSource.name}`,
                           },
                       ]
                     : []),
