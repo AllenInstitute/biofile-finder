638b6af6e735718c30230f13b798e1370c0fc8c5
Peyton Lee <peyton.lee@alleninstitute.org>
2026-09-14 16:53:32 -0700

    refactor: Update props for StatusMessageCard

--- packages/core/components/StatusMessageCard/index.tsx
+++ packages/core/components/StatusMessageCard/index.tsx
@@ -2,17 +2,15 @@ import * as React from "react";
 
 import styles from "./StatusMessageCard.module.css";
 
-/** Displays a warning or error message onscreen, as a block of text. */
-export default function StatusMessageCard({
-    type,
-    children,
-}: {
+type StatusMessageCardProps = {
     type: "warning" | "error";
-    children: React.ReactNode;
-}) {
+};
+
+/** Displays a warning or error message onscreen, as a block of text. */
+export default function StatusMessageCard(props: React.PropsWithChildren<StatusMessageCardProps>) {
     return (
-        <p className={type === "warning" ? styles.warningMessage : styles.errorMessage}>
-            {children}
+        <p className={props.type === "warning" ? styles.warningMessage : styles.errorMessage}>
+            {props.children}
         </p>
     );
 }
