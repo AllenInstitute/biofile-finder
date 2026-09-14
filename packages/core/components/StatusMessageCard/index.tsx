2de445d01a78bd65c690546aff7712d029822078
Peyton Lee <peyton.lee@alleninstitute.org>
2026-09-14 15:42:43 -0700

    feat: Make separate component for status messages

---
+++ packages/core/components/StatusMessageCard/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+import styles from "./StatusMessageCard.module.css";
+
+/** Displays a warning or error message onscreen, as a block of text. */
+export default function StatusMessageInline({
+    type,
+    children,
+}: {
+    type: "warning" | "error";
+    children: React.ReactNode;
+}) {
+    return (
+        <p className={type === "warning" ? styles.warningMessage : styles.errorMessage}>
+            {children}
+        </p>
+    );
+}
